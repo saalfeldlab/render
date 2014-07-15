1c4d76266b6666102ee728f012509c4ff210105d
Eric Trautman <trautmane@janelia.hhmi.org>
2014-07-15 15:25:42 -0400

    enable render test to run in any environment (remove local path dependencies)

--- render-app/src/test/java/org/janelia/alignment/RenderTest.java
+++ render-app/src/test/java/org/janelia/alignment/RenderTest.java
@@ -23,12 +23,20 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Tests the {@link Render} class.
@@ -37,31 +45,31 @@ import java.util.Date;
  */
 public class RenderTest {
 
+    private String modulePath;
     private File outputFile;
+    private File tileSpecFile;
 
     @Before
     public void setup() throws Exception {
         final SimpleDateFormat TIMESTAMP = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-        outputFile = new File("test-render-" + TIMESTAMP.format(new Date()) +".jpg").getCanonicalFile();
+        final String timestamp = TIMESTAMP.format(new Date());
+        outputFile = new File("test-render-" + timestamp +".jpg").getCanonicalFile();
+        modulePath = outputFile.getParentFile().getCanonicalPath();
+        tileSpecFile = new File("test-render-" + timestamp +".json").getCanonicalFile();
+        buildTileSpecFile();
     }
 
     @After
     public void tearDown() throws Exception {
-        if ((outputFile != null) && outputFile.exists()) {
-            if (outputFile.delete()) {
-                LOG.info("tearDown: deleted " + outputFile.getAbsolutePath());
-            } else {
-                LOG.info("tearDown: failed to delete " + outputFile.getAbsolutePath());
-            }
-        }
+        deleteTestFile(outputFile);
+        deleteTestFile(tileSpecFile);
     }
 
     @Test
     public void testMain() throws Exception {
 
-        final File workDir = outputFile.getParentFile();
-        final String workPath = workDir.getCanonicalPath();
-        final File tileSpecFile = new File(workPath + "/src/test/resources/stitch-test/test_4_tiles.json");
+        final File expectedFile =
+                new File(modulePath + "/src/test/resources/stitch-test/expected_stitched_4_tiles.jpg");
 
         final String[] args = {
                 "--url", tileSpecFile.toURI().toString(),
@@ -75,8 +83,6 @@ public class RenderTest {
 
         Assert.assertTrue("stitched file " + outputFile.getAbsolutePath() + " not created", outputFile.exists());
 
-        final File stitchTestDir = tileSpecFile.getParentFile();
-        final File expectedFile = new File(stitchTestDir, "expected_stitched_4_tiles.jpg");
         final String expectedDigestString = getDigestString(expectedFile);
         final String actualDigestString = getDigestString(outputFile);
 
@@ -103,5 +109,50 @@ public class RenderTest {
         return sb.toString();
     }
 
+    private void buildTileSpecFile() throws Exception {
+        final File templateSpecFile = new File(modulePath + "/src/test/resources/stitch-test/test_4_tiles.json");
+
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        try {
+            reader = new BufferedReader(new FileReader(templateSpecFile));
+            writer = new BufferedWriter(new FileWriter(tileSpecFile));
+            String line;
+            Matcher m;
+            while ((line = reader.readLine()) != null) {
+                m = MODULE_DIR_PATTERN.matcher(line);
+                line = m.replaceAll(modulePath);
+                writer.write(line);
+                writer.write('\n');
+            }
+        } finally {
+            closeFileStream(reader, templateSpecFile);
+            closeFileStream(writer, tileSpecFile);
+        }
+    }
+
+    private void closeFileStream(Closeable closable,
+                                 File context) {
+        if (closable != null) {
+            try {
+                closable.close();
+            } catch (IOException e) {
+                LOG.warn("failed to close " + context.getAbsolutePath());
+            }
+        }
+    }
+
+    private void deleteTestFile(File file) {
+        if ((file != null) && file.exists()) {
+            if (file.delete()) {
+                LOG.info("deleteTestFile: deleted " + file.getAbsolutePath());
+            } else {
+                LOG.info("deleteTestFile: failed to delete " + file.getAbsolutePath());
+            }
+        }
+    }
+
     private static final Logger LOG = LoggerFactory.getLogger(RenderTest.class);
+
+    private static final Pattern MODULE_DIR_PATTERN = Pattern.compile("\\$\\{MODULE_DIR\\}");
 }
