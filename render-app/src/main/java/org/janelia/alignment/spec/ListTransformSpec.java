880dba7728e41abacb1e694721145aea481a9c1f
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2015-05-29 16:01:18 -0400

    added method to create a TransformSpec from a CoordinateTransform

--- render-app/src/main/java/org/janelia/alignment/spec/ListTransformSpec.java
+++ render-app/src/main/java/org/janelia/alignment/spec/ListTransformSpec.java
@@ -1,13 +1,13 @@
 package org.janelia.alignment.spec;
 
-import mpicbg.models.CoordinateTransform;
-import mpicbg.models.CoordinateTransformList;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import mpicbg.models.CoordinateTransform;
+import mpicbg.models.CoordinateTransformList;
 
 /**
  * List of transform specifications.
@@ -29,23 +29,23 @@ public class ListTransformSpec extends TransformSpec {
         this(null, null);
     }
 
-    public ListTransformSpec(String id,
-                             TransformSpecMetaData metaData) {
+    public ListTransformSpec(final String id,
+                             final TransformSpecMetaData metaData) {
         super(id, TYPE, metaData);
         this.specList = new ArrayList<TransformSpec>();
     }
 
-    public TransformSpec getSpec(int index) {
+    public TransformSpec getSpec(final int index) {
         return specList.get(index);
     }
 
-    public void addSpec(TransformSpec spec) {
+    public void addSpec(final TransformSpec spec) {
         specList.add(spec);
         removeInstance();
     }
 
-    public void setSpec(int index,
-                        TransformSpec spec) {
+    public void setSpec(final int index,
+                        final TransformSpec spec) {
         specList.set(index, spec);
         removeInstance();
     }
@@ -57,7 +57,7 @@ public class ListTransformSpec extends TransformSpec {
         }
     }
 
-    public void addAllSpecs(List<TransformSpec> specs) {
+    public void addAllSpecs(final List<TransformSpec> specs) {
         this.specList.addAll(specs);
         removeInstance();
     }
@@ -68,7 +68,7 @@ public class ListTransformSpec extends TransformSpec {
 
     public void removeNullSpecs() {
         TransformSpec spec;
-        for (Iterator<TransformSpec> i = specList.iterator(); i.hasNext();) {
+        for (final Iterator<TransformSpec> i = specList.iterator(); i.hasNext();) {
             spec = i.next();
             if (spec == null) {
                 i.remove();
@@ -81,7 +81,7 @@ public class ListTransformSpec extends TransformSpec {
     public boolean isFullyResolved()
             throws IllegalStateException {
         boolean allSpecsResolved = true;
-        for (TransformSpec spec : specList) {
+        for (final TransformSpec spec : specList) {
             if (spec == null) {
                 throw new IllegalStateException("A null spec is part of the transform spec list with id '" + getId() +
                                                 "'.  Check for an extraneous comma at the end of the list.");
@@ -95,22 +95,22 @@ public class ListTransformSpec extends TransformSpec {
     }
 
     @Override
-    public void addUnresolvedIds(Set<String> unresolvedIds) {
-        for (TransformSpec spec : specList) {
+    public void addUnresolvedIds(final Set<String> unresolvedIds) {
+        for (final TransformSpec spec : specList) {
             spec.addUnresolvedIds(unresolvedIds);
         }
     }
 
     @Override
-    public void resolveReferences(Map<String, TransformSpec> idToSpecMap) {
-        for (TransformSpec spec : specList) {
+    public void resolveReferences(final Map<String, TransformSpec> idToSpecMap) {
+        for (final TransformSpec spec : specList) {
             spec.resolveReferences(idToSpecMap);
         }
     }
 
     @Override
-    public void flatten(ListTransformSpec flattenedList) throws IllegalStateException {
-        for (TransformSpec spec : specList) {
+    public void flatten(final ListTransformSpec flattenedList) throws IllegalStateException {
+        for (final TransformSpec spec : specList) {
             spec.flatten(flattenedList);
         }
     }
@@ -125,7 +125,7 @@ public class ListTransformSpec extends TransformSpec {
     protected CoordinateTransform buildInstance()
             throws IllegalArgumentException {
         final CoordinateTransformList<CoordinateTransform> ctList = new CoordinateTransformList<CoordinateTransform>();
-        for (TransformSpec spec : specList) {
+        for (final TransformSpec spec : specList) {
             ctList.add(spec.buildInstance());
         }
         return ctList;
--- render-app/src/main/java/org/janelia/alignment/spec/TransformSpec.java
+++ render-app/src/main/java/org/janelia/alignment/spec/TransformSpec.java
@@ -1,7 +1,5 @@
 package org.janelia.alignment.spec;
 
-import com.google.gson.reflect.TypeToken;
-
 import java.io.Reader;
 import java.io.Serializable;
 import java.lang.reflect.Type;
@@ -9,10 +7,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import mpicbg.models.CoordinateTransform;
+import mpicbg.models.CoordinateTransformList;
+import mpicbg.models.InterpolatedCoordinateTransform;
 
 import org.janelia.alignment.json.JsonUtils;
+
+import com.google.gson.reflect.TypeToken;
 
 /**
  * Abstract base for all transformation specifications.
@@ -137,11 +140,11 @@ public abstract class TransformSpec implements Serializable {
         return JsonUtils.GSON.toJson(this);
     }
 
-    public static List<TransformSpec> fromJsonArray(String json) {
+    public static List<TransformSpec> fromJsonArray(final String json) {
         return JsonUtils.GSON.fromJson(json, LIST_TYPE);
     }
 
-    public static List<TransformSpec> fromJsonArray(Reader json) {
+    public static List<TransformSpec> fromJsonArray(final Reader json) {
         return JsonUtils.GSON.fromJson(json, LIST_TYPE);
     }
 
@@ -162,5 +165,36 @@ public abstract class TransformSpec implements Serializable {
     }
 
     private static final Type LIST_TYPE = new TypeToken<List<TransformSpec>>(){}.getType();
+
+    /**
+     * Create a TransformSpec from a {@link CoordinateTransform}.  The
+     * {@link CoordinateTransform} has to be a {@link CoordinateTransformList},
+     * an {@link InterpolatedCoordinateTransform} or a TrakEM2 compatible
+     * {@link mpicbg.trakem2.transform.CoordinateTransform}.  Otherwise the
+     * method returns null.
+     *
+     * @param transform
+     * @return
+     */
+    static public TransformSpec create(final CoordinateTransform transform) {
+        if (CoordinateTransformList.class.isInstance(transform)) {
+            final ListTransformSpec listSpec = new ListTransformSpec(UUID.randomUUID().toString(), null);
+            @SuppressWarnings({ "rawtypes", "unchecked" })
+            final List<CoordinateTransform> transforms = ((CoordinateTransformList)transform).getList(null);
+            for (final CoordinateTransform t : transforms)
+                listSpec.addSpec(create(t));
+            return listSpec;
+        } else if (InterpolatedCoordinateTransform.class.isInstance(transform)) {
+            @SuppressWarnings("rawtypes")
+            final InterpolatedCoordinateTransform ab = (InterpolatedCoordinateTransform)transform;
+            final CoordinateTransform a = ab.getA();
+            final CoordinateTransform b = ab.getB();
+            return new InterpolatedTransformSpec(UUID.randomUUID().toString(), null, create(a), create(b), ab.getLambda());
+        } else if (mpicbg.trakem2.transform.CoordinateTransform.class.isInstance(transform)) {
+            final mpicbg.trakem2.transform.CoordinateTransform t = (mpicbg.trakem2.transform.CoordinateTransform)transform;
+            return new LeafTransformSpec(UUID.randomUUID().toString(), null, t.getClass().getCanonicalName(), t.toDataString());
+            //return new LeafTransformSpec(UUID.randomUUID().toString(), null, t.getClass().getCanonicalName(), null);
+        } else return null;
+    }
 
 }
