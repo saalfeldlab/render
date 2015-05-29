99d75ca6da0cfa06157d90ad926c80efa8502b3a
Eric Trautman <trautmane@janelia.hhmi.org>
2015-05-29 11:09:30 -0400

    split out read only render dao tests to improve build time

--- render-ws/src/test/java/org/janelia/render/service/dao/RenderDaoTest.java
+++ render-ws/src/test/java/org/janelia/render/service/dao/RenderDaoTest.java
@@ -1,29 +1,20 @@
 package org.janelia.render.service.dao;
 
-import com.google.gson.reflect.TypeToken;
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import mpicbg.trakem2.transform.AffineModel2D;
 
-import org.janelia.alignment.RenderParameters;
-import org.janelia.alignment.json.JsonUtils;
 import org.janelia.alignment.spec.Bounds;
 import org.janelia.alignment.spec.LayoutData;
 import org.janelia.alignment.spec.LeafTransformSpec;
 import org.janelia.alignment.spec.ListTransformSpec;
 import org.janelia.alignment.spec.ReferenceTransformSpec;
-import org.janelia.alignment.spec.TileBounds;
-import org.janelia.alignment.spec.TileCoordinates;
 import org.janelia.alignment.spec.TileSpec;
 import org.janelia.alignment.spec.TransformSpec;
 import org.janelia.alignment.spec.TransformSpecMetaData;
-import org.janelia.render.service.model.ObjectNotFoundException;
 import org.janelia.alignment.spec.stack.StackId;
 import org.janelia.alignment.spec.stack.StackMetaData;
 import org.janelia.alignment.spec.stack.StackStats;
@@ -34,13 +25,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.janelia.alignment.spec.stack.StackMetaData.StackState.LOADING;
 
 /**
- * Tests the {@link RenderDao} class.
+ * Tests {@link RenderDao} methods that change persisted data.
+ * The embedded database is rebuilt for each test, so these tests take longer to run.
  *
  * @author Eric Trautman
  */
@@ -81,27 +71,6 @@ public class RenderDaoTest {
     @AfterClass
     public static void after() throws Exception {
         embeddedMongoDb.stop();
-    }
-
-    @Test
-    public void testGetStackMetaDataListForOwner() throws Exception {
-        final List<StackMetaData> list = dao.getStackMetaDataListForOwner(stackId.getOwner());
-
-        Assert.assertNotNull("null list retrieved", list);
-        Assert.assertEquals("invalid number of stacks found", 2, list.size());
-    }
-
-    @Test
-    public void testGetStackMetaData() throws Exception {
-
-        final Integer expectedLayoutWidth = 2600;
-        final Integer expectedLayoutHeight = 2200;
-
-        final StackMetaData stackMetaData = dao.getStackMetaData(stackId);
-
-        Assert.assertNotNull("null stack meta data retrieved", stackMetaData);
-        Assert.assertEquals("invalid layout width", expectedLayoutWidth, stackMetaData.getLayoutWidth());
-        Assert.assertEquals("invalid layout height", expectedLayoutHeight, stackMetaData.getLayoutHeight());
     }
 
     @Test
@@ -221,63 +190,6 @@ public class RenderDaoTest {
     }
 
     @Test
-    public void testGetParameters() throws Exception {
-
-        final Double x = 1000.0;
-        final Double y = 3000.0;
-        final Double z = 3903.0;
-        final Integer width = 5000;
-        final Integer height = 2000;
-        final Double scale = 0.5;
-
-        final RenderParameters parameters = dao.getParameters(stackId, x, y, z, width, height, scale);
-
-        Assert.assertNotNull("null parameters retrieved", parameters);
-        Assert.assertEquals("invalid width parsed", width.intValue(), parameters.getWidth());
-
-        // validate that dao parameters can be re-serialized
-        try {
-            final String json = parameters.toJson();
-            Assert.assertNotNull("null json string produced for parameters", json);
-        } catch (final Exception e) {
-            LOG.error("failed to serialize json for " + parameters, e);
-            Assert.fail("retrieved parameters cannot be re-serialized to json");
-        }
-
-        parameters.initializeDerivedValues();
-        final List<TileSpec> tileSpecs = parameters.getTileSpecs();
-        Assert.assertNotNull("null tile specs value after init", tileSpecs);
-        Assert.assertEquals("invalid number of tiles after init", 6, tileSpecs.size());
-
-        ListTransformSpec transforms;
-        for (final TileSpec tileSpec : tileSpecs) {
-            transforms = tileSpec.getTransforms();
-            Assert.assertTrue("tileSpec " + tileSpec.getTileId() + " is not fully resolved",
-                              transforms.isFullyResolved());
-        }
-    }
-
-    @Test
-    public void testGetTileSpec() throws Exception {
-        final String existingTileId = "134";
-        final TileSpec tileSpec = dao.getTileSpec(stackId, existingTileId, false);
-        Assert.assertNotNull("null tileSpec retrieved", tileSpec);
-        Assert.assertEquals("invalid tileId retrieved", existingTileId, tileSpec.getTileId());
-    }
-
-    @Test
-    public void testGetTileSpecs() throws Exception {
-        final List<TileSpec> list = dao.getTileSpecs(stackId, 3903.0);
-        Assert.assertNotNull("null tile spec list retrieved", list);
-        Assert.assertEquals("invalid number of tile specs retrieved", 12, list.size());
-    }
-
-    @Test(expected = ObjectNotFoundException.class)
-    public void testGetTileSpecWithBadId() throws Exception {
-        dao.getTileSpec(stackId, "missingId", false);
-    }
-
-    @Test
     public void testSaveTileSpec() throws Exception {
         final String tileId = "new-tile-1";
         final String temca = "0";
@@ -313,30 +225,6 @@ public class RenderDaoTest {
         Assert.assertNotNull("null layout retrieved after update", updatedLayoutData);
         Assert.assertEquals("invalid temca retrieved after update", changedTemca, updatedLayoutData.getTemca());
         Assert.assertTrue("tileSpec is missing transforms after update", tileSpec.hasTransforms());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testSaveTileSpecWithBadTransformReference() throws Exception {
-        final TileSpec tileSpec = new TileSpec();
-        tileSpec.setZ(12.3);
-        tileSpec.setTileId("bad-ref-tile");
-        final List<TransformSpec> list = new ArrayList<>();
-        list.add(new ReferenceTransformSpec("missing-id"));
-        tileSpec.addTransformSpecs(list);
-
-        dao.saveTileSpec(stackId, tileSpec);
-    }
-
-    @Test
-    public void testGetTransformSpec() throws Exception {
-        final TransformSpec transformSpec = dao.getTransformSpec(stackId, "2");
-        Assert.assertNotNull("null transformSpec retrieved", transformSpec);
-        Assert.assertEquals("invalid type retrieved", "list", transformSpec.getType());
-    }
-
-    @Test(expected = ObjectNotFoundException.class)
-    public void testGetTransformSpecWithBadId() throws Exception {
-        dao.getTransformSpec(stackId, "missingId");
     }
 
     @Test
@@ -378,104 +266,6 @@ public class RenderDaoTest {
         Assert.assertFalse("transformSpec should not be resolved after update", updatedSpec.isFullyResolved());
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testSaveTransformSpecWithBadTransformReference() throws Exception {
-        final ListTransformSpec listSpec = new ListTransformSpec("bad-ref-transform", null);
-        listSpec.addSpec(new ReferenceTransformSpec("missing-id"));
-
-        dao.saveTransformSpec(stackId, listSpec);
-    }
-
-    @Test
-    public void testGetZValues() throws Exception {
-        final List<Double> list = dao.getZValues(stackId);
-
-        Assert.assertNotNull("null list retrieved", list);
-        Assert.assertEquals("invalid number of layers found", 1, list.size());
-    }
-
-    @Test
-    public void testGetLayerBounds() throws Exception {
-        final Double expectedMinX = 1094.0;
-        final Double expectedMinY = 1769.0;
-        final Double expectedMaxX = 9917.0;
-        final Double expectedMaxY = 8301.0;
-
-        final Double z = 3903.0;
-
-        Bounds bounds = dao.getLayerBounds(stackId, z);
-
-        Assert.assertNotNull("null layer bounds retrieved", bounds);
-        Assert.assertEquals("invalid layer minX", expectedMinX, bounds.getMinX(), BOUNDS_DELTA);
-        Assert.assertEquals("invalid layer minY", expectedMinY, bounds.getMinY(), BOUNDS_DELTA);
-        Assert.assertEquals("invalid layer minZ", z, bounds.getMinZ(), BOUNDS_DELTA);
-        Assert.assertEquals("invalid layer maxX", expectedMaxX, bounds.getMaxX(), BOUNDS_DELTA);
-        Assert.assertEquals("invalid layer maxY", expectedMaxY, bounds.getMaxY(), BOUNDS_DELTA);
-        Assert.assertEquals("invalid layer maxZ", z, bounds.getMaxZ(), BOUNDS_DELTA);
-    }
-
-    @Test
-    public void testGetTileBounds() throws Exception {
-        final Double z = 3903.0;
-        final List<TileBounds> list = dao.getTileBounds(stackId, z);
-
-        Assert.assertNotNull("null list retrieved", list);
-        Assert.assertEquals("invalid number of tiles found", 12, list.size());
-
-        final TileBounds firstTileBounds = list.get(0);
-
-        Assert.assertNotNull("null bounds for first tile", firstTileBounds);
-        Assert.assertEquals("incorrect id for first tile", "134", firstTileBounds.getTileId());
-        Assert.assertTrue("bound box not defined for first tile", firstTileBounds.isBoundingBoxDefined());
-    }
-
-    @Test
-    public void testWriteCoordinatesWithTileIds() throws Exception {
-        final Double z = 3903.0;
-        final List<TileCoordinates> worldCoordinates = new ArrayList<>();
-        worldCoordinates.add(TileCoordinates.buildWorldInstance(null, new double[] {1900, 3000}));
-        worldCoordinates.add(TileCoordinates.buildWorldInstance(null, new double[] {3700, 3000}));
-        worldCoordinates.add(TileCoordinates.buildWorldInstance(null, new double[]{4500, 3000}));
-
-        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream(1024);
-
-        dao.writeCoordinatesWithTileIds(stackId, z, worldCoordinates, outputStream);
-
-        final String json = outputStream.toString();
-        final Type typeOfT = new TypeToken<List<List<TileCoordinates>>>(){}.getType();
-        final List<List<TileCoordinates>> worldCoordinatesWithTileIds = JsonUtils.GSON.fromJson(json, typeOfT);
-
-        Assert.assertEquals("invalid number of lists returned",
-                            worldCoordinates.size(), worldCoordinatesWithTileIds.size());
-
-        // first coordinate
-        List<TileCoordinates> tileCoordinatesList = worldCoordinatesWithTileIds.get(0);
-
-        Assert.assertEquals("invalid number of tiles found for first coordinate",
-                            1, tileCoordinatesList.size());
-
-        TileCoordinates tileCoordinates = tileCoordinatesList.get(0);
-
-        Assert.assertEquals("invalid tileId for first coordinate",
-                            "134", tileCoordinates.getTileId());
-
-        // second coordinate
-        tileCoordinatesList = worldCoordinatesWithTileIds.get(1);
-
-        Assert.assertEquals("invalid number of tiles found for second coordinate",
-                            2, tileCoordinatesList.size());
-
-        tileCoordinates = tileCoordinatesList.get(0);
-
-        Assert.assertEquals("invalid tileId for second coordinate, first tile",
-                            "134", tileCoordinates.getTileId());
-
-        tileCoordinates = tileCoordinatesList.get(1);
-
-        Assert.assertEquals("invalid tileId for second coordinate, second tile",
-                            "171", tileCoordinates.getTileId());
-    }
-
     public static void validateStackMetaData(String context,
                                              StackMetaData.StackState expectedState,
                                              Integer expectedVersionNumber,
@@ -512,6 +302,4 @@ public class RenderDaoTest {
                             expectedVersion.getMipmapMetaData(), actualVersion.getMipmapMetaData());
     }
 
-    private static final Logger LOG = LoggerFactory.getLogger(RenderDaoTest.class);
-    private static final Double BOUNDS_DELTA = 0.1;
 }
