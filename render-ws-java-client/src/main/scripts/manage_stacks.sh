e3324d2b4f25edd200561195d10b45e62495ea7a
Eric Trautman <trautmane@janelia.hhmi.org>
2016-04-15 17:48:40 -0400

    always include memory value when invoking setup_java_env.sh

--- render-ws-java-client/src/main/scripts/manage_stacks.sh
+++ render-ws-java-client/src/main/scripts/manage_stacks.sh
@@ -2,6 +2,6 @@
 
 ABSOLUTE_SCRIPT=`readlink -m $0`
 SCRIPTS_DIR=`dirname ${ABSOLUTE_SCRIPT}`
-. ${SCRIPTS_DIR}/setup_java_env.sh
+. ${SCRIPTS_DIR}/setup_java_env.sh 1G
 
 runJavaCommandAndExit org.janelia.render.client.StackClient $*
