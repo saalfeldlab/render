27e2a306162aabdec09c7963b32fe1671c2b1627
Eric Trautman <trautmane@janelia.hhmi.org>
2016-07-05 17:56:16 -0400

    add validator and target parameters to transform section client

--- render-ws-java-client/src/main/java/org/janelia/render/client/TransformSectionClient.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/TransformSectionClient.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.janelia.alignment.spec.LeafTransformSpec;
 import org.janelia.alignment.spec.ResolvedTileSpecCollection;
-import org.janelia.alignment.spec.validator.TemTileSpecValidator;
 import org.janelia.alignment.spec.validator.TileSpecValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +25,18 @@ public class TransformSectionClient {
         @Parameter(names = "--stack", description = "Stack name", required = true)
         private String stack;
 
+        @Parameter(
+                names = "--targetProject",
+                description = "Name of target project that will contain transformed tiles (default is to reuse source project)",
+                required = false)
+        private String targetProject;
+
+        @Parameter(
+                names = "--targetStack",
+                description = "Name of target stack that will contain transformed tiles (default is to reuse source stack)",
+                required = false)
+        private String targetStack;
+
         @Parameter(names = "--transformId", description = "Identifier for tranformation", required = true)
         private String transformId;
 
@@ -36,14 +47,18 @@ public class TransformSectionClient {
         @Parameter(names = "--transformData", description = "Data with which transformation implementation should be initialized (expects values to be separated by ',' instead of ' ')", required = true)
         private String transformData;
 
-        @Parameter(names = "--disableValidation", description = "Disable flyTEM tile validation", required = false, arity = 0)
-        private boolean disableValidation;
-
         @Parameter(names = "--replaceLast", description = "Replace each tile's last transform with this one (default is to append new transform)", required = false, arity = 0)
         private boolean replaceLast;
 
         @Parameter(description = "Z values", required = true)
         private List<String> zValues;
+
+        public String getTargetStack() {
+            if ((targetStack == null) || (targetStack.trim().length() == 0)) {
+                targetStack = stack;
+            }
+            return targetStack;
+        }
     }
 
     public static void main(final String[] args) {
@@ -69,7 +84,8 @@ public class TransformSectionClient {
     private final TileSpecValidator tileSpecValidator;
     private final LeafTransformSpec stackTransform;
 
-    private final RenderDataClient renderDataClient;
+    private final RenderDataClient sourceRenderDataClient;
+    private final RenderDataClient targetRenderDataClient;
 
     public TransformSectionClient(final Parameters parameters) {
 
@@ -78,17 +94,24 @@ public class TransformSectionClient {
         this.stackTransform = new LeafTransformSpec(parameters.transformId,
                                                     null,
                                                     parameters.transformClass,
-                                                    parameters.transformData.replace(',',' '));
+                                                    parameters.transformData.replace(',', ' '));
 
-        if (parameters.disableValidation) {
-            this.tileSpecValidator = null;
+        this.tileSpecValidator = parameters.getValidatorInstance();
+
+        this.sourceRenderDataClient = new RenderDataClient(parameters.baseDataUrl,
+                                                           parameters.owner,
+                                                           parameters.project);
+
+        if ((parameters.targetProject == null) ||
+            (parameters.targetProject.trim().length() == 0) ||
+            (parameters.targetProject.equals(parameters.project))) {
+            this.targetRenderDataClient = sourceRenderDataClient;
         } else {
-            this.tileSpecValidator = new TemTileSpecValidator();
+            this.targetRenderDataClient = new RenderDataClient(parameters.baseDataUrl,
+                                                               parameters.owner,
+                                                               parameters.targetProject);
         }
 
-        this.renderDataClient = new RenderDataClient(parameters.baseDataUrl,
-                                                     parameters.owner,
-                                                     parameters.project);
     }
 
     public void generateStackDataForZ(final Double z)
@@ -96,7 +119,7 @@ public class TransformSectionClient {
 
         LOG.info("generateStackDataForZ: entry, z={}", z);
 
-        final ResolvedTileSpecCollection tiles = renderDataClient.getResolvedTiles(parameters.stack, z);
+        final ResolvedTileSpecCollection tiles = sourceRenderDataClient.getResolvedTiles(parameters.stack, z);
 
         tiles.addTransformSpecToCollection(stackTransform);
         tiles.addReferenceTransformToAllTiles(stackTransform.getId(), parameters.replaceLast);
@@ -111,7 +134,7 @@ public class TransformSectionClient {
         LOG.info("generateStackDataForZ: added transform and derived bounding boxes for {} tiles with z of {}, removed {} bad tiles",
                  totalNumberOfTiles, z, numberOfRemovedTiles);
 
-        renderDataClient.saveResolvedTiles(tiles, parameters.stack, z);
+        targetRenderDataClient.saveResolvedTiles(tiles, parameters.getTargetStack(), z);
 
         LOG.info("generateStackDataForZ: exit, saved tiles and transforms for {}", z);
     }
