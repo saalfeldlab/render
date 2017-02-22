88d8c3085a7f52bcbcca44893760199e02c6e80d
Eric Trautman <trautmane@janelia.hhmi.org>
2017-02-21 20:57:25 -0500

    added --replaceLastTransformWithStage and --splitMergedSections options to the java CopyStackClient

--- render-ws-java-client/src/main/java/org/janelia/render/client/CopyStackClient.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/CopyStackClient.java
@@ -2,17 +2,31 @@ package org.janelia.render.client;
 
 import com.beust.jcommander.Parameter;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import mpicbg.trakem2.transform.AffineModel2D;
+
+import org.janelia.alignment.spec.LayoutData;
+import org.janelia.alignment.spec.LeafTransformSpec;
 import org.janelia.alignment.spec.ResolvedTileSpecCollection;
+import org.janelia.alignment.spec.SectionData;
 import org.janelia.alignment.spec.TileBounds;
 import org.janelia.alignment.spec.TileBoundsRTree;
+import org.janelia.alignment.spec.TileSpec;
+import org.janelia.alignment.spec.TransformSpec;
 import org.janelia.alignment.spec.stack.StackMetaData;
 import org.janelia.alignment.spec.stack.StackMetaData.StackState;
 import org.janelia.alignment.spec.stack.StackVersion;
+import org.janelia.alignment.util.ProcessTimer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -72,6 +86,18 @@ public class CopyStackClient {
                 description = "Complete the to stack after copying all layers",
                 required = false, arity = 0)
         private boolean completeToStackAfterCopy = false;
+
+        @Parameter(
+                names = "--replaceLastTransformWithStage",
+                description = "Replace the last transform in each tile space with a 'stage identity' transform",
+                required = false, arity = 0)
+        private boolean replaceLastTransformWithStage = false;
+
+        @Parameter(
+                names = "--splitMergedSections",
+                description = "Reset z values for tiles so that original sections are separated",
+                required = false, arity = 0)
+        private boolean splitMergedSections = false;
 
         public String getToOwner() {
             if (toOwner == null) {
@@ -139,6 +165,7 @@ public class CopyStackClient {
     private final Parameters parameters;
     private final RenderDataClient fromDataClient;
     private final RenderDataClient toDataClient;
+    private final Map<String, Integer> sectionIdToZMap;
 
     public CopyStackClient(final Parameters parameters) throws Exception {
 
@@ -151,6 +178,12 @@ public class CopyStackClient {
         this.toDataClient = new RenderDataClient(parameters.baseDataUrl,
                                                  parameters.getToOwner(),
                                                  parameters.getToProject());
+
+        if (parameters.splitMergedSections) {
+            this.sectionIdToZMap = getSectionIdToIntegralZMap();
+        } else {
+            this.sectionIdToZMap = null;
+        }
     }
 
     public void setUpToStack() throws Exception {
@@ -198,13 +231,33 @@ public class CopyStackClient {
             sourceCollection.filterSpecs(tileIdsToKeep);
         }
 
+        if (parameters.replaceLastTransformWithStage) {
+            replaceLastTransformWithStage(sourceCollection);
+        }
+
+        final Set<Double> toStackZValues = new LinkedHashSet<>();
+        if (parameters.splitMergedSections) {
+            for (final TileSpec tileSpec : sourceCollection.getTileSpecs()) {
+                final Double zValue = new Double(sectionIdToZMap.get(tileSpec.getLayout().getSectionId()));
+                toStackZValues.add(zValue);
+                tileSpec.setZ(zValue);
+            }
+
+            LOG.info("copyLayer: updated z values for {} tiles",
+                     sourceCollection.getTileCount());
+        } else {
+            toStackZValues.add(z);
+        }
+
         sourceCollection.removeUnreferencedTransforms();
 
         if (! parameters.keepExisting) {
-            toDataClient.deleteStack(parameters.toStack, z);
+            for (final Double zValue : toStackZValues) {
+                toDataClient.deleteStack(parameters.toStack, zValue);
+            }
         }
 
-        toDataClient.saveResolvedTiles(sourceCollection, parameters.toStack, z);
+        toDataClient.saveResolvedTiles(sourceCollection, parameters.toStack, null);
     }
 
     private Set<String> getIdsForTilesInBox(final Double z) throws Exception {
@@ -225,6 +278,85 @@ public class CopyStackClient {
         }
 
         return tileIdsToKeep;
+    }
+
+    private void replaceLastTransformWithStage(final ResolvedTileSpecCollection sourceCollection) {
+
+        final ProcessTimer timer = new ProcessTimer();
+
+        int tileSpecCount = 0;
+
+        for (final TileSpec tileSpec : sourceCollection.getTileSpecs()) {
+
+            final LayoutData layoutData = tileSpec.getLayout();
+            final String dataString = "1 0 0 1 " + layoutData.getStageX() + " " + layoutData.getStageY();
+            final TransformSpec transformSpec = new LeafTransformSpec(AffineModel2D.class.getName(),
+                                                                      dataString);
+
+            sourceCollection.addTransformSpecToTile(tileSpec.getTileId(),
+                                                    transformSpec,
+                                                    true);
+
+            tileSpecCount++;
+
+            if (timer.hasIntervalPassed()) {
+                LOG.info("replaceLastTransformWithStage: updated transforms for {} out of {} tiles",
+                         tileSpecCount, sourceCollection.getTileCount());
+            }
+        }
+
+        LOG.info("replaceLastTransformWithStage: exit, updated transforms for {} tiles",
+                 tileSpecCount);
+    }
+
+    private Map<String, Integer> getSectionIdToIntegralZMap()
+            throws IOException {
+
+        final Comparator<SectionData> sectionComparator = new Comparator<SectionData>() {
+            @Override
+            public int compare(final SectionData o1,
+                               final SectionData o2) {
+                int result = o1.getZ().compareTo(o2.getZ());
+                if (result == 0) {
+                    result = o1.getSectionId().compareTo(o2.getSectionId());
+                }
+                return result;
+            }
+        };
+
+        final List<SectionData> orderedSectionDataList =
+                fromDataClient.getStackSectionData(parameters.fromStack, null, null);
+
+        Collections.sort(orderedSectionDataList,
+                         sectionComparator);
+
+        final Map<String, Integer> sectionIdToZMap = new HashMap<>(orderedSectionDataList.size());
+
+        final int firstContrivedZ;
+        if (orderedSectionDataList.size() > 0) {
+
+            // highlight contrived z values by making them abnormally large and ensuring no overlap with real z values
+            final Double lastZ = orderedSectionDataList.get(orderedSectionDataList.size() - 1).getZ();
+            if (lastZ < 50000) {
+                firstContrivedZ = 100000;
+            } else {
+                firstContrivedZ = lastZ.intValue() + 50000;
+            }
+
+            for (int i = 0; i < orderedSectionDataList.size(); i++) {
+                sectionIdToZMap.put(orderedSectionDataList.get(i).getSectionId(), i + firstContrivedZ);
+            }
+
+        } else {
+            firstContrivedZ = 0;
+        }
+
+        final int lastContrivedZ = firstContrivedZ + sectionIdToZMap.size() - 1;
+
+        LOG.info("getSectionIdToIntegralZMap: exit, mapped {} sections to z values {} - {}",
+                 sectionIdToZMap.size(), firstContrivedZ, lastContrivedZ);
+
+        return sectionIdToZMap;
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(CopyStackClient.class);
