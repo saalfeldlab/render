45067b4561ac997f109aa435d21d58e99733f0ee
Eric Trautman <trautmane@janelia.hhmi.org>
2017-10-25 16:05:18 -0400

    second attempt at s3 connection that can handle contentType retrieval

--- render-app/src/main/java/org/janelia/alignment/protocol/s3/S3Handler.java
+++ render-app/src/main/java/org/janelia/alignment/protocol/s3/S3Handler.java
@@ -1,8 +1,14 @@
 package org.janelia.alignment.protocol.s3;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.kms.model.UnsupportedOperationException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
+import java.io.IOException;
 import java.net.URLStreamHandler;
 import java.net.URLConnection;
 
@@ -10,18 +16,19 @@ import java.net.URLConnection;
  * A java protocol handler for s3:// URLs.
  */
 public class S3Handler extends URLStreamHandler {
-    private AWSCredentialsProvider credentialsProvider;
+
+    private final AmazonS3 s3Client;
 
     public S3Handler() {
-        this(new DefaultAWSCredentialsProviderChain());
+        throw new UnsupportedOperationException("lazy load of s3 client needs to be re-thought if we need this default constructor");
     }
 
-    public S3Handler(AWSCredentialsProvider credentialsProvider) {
-        this.credentialsProvider = credentialsProvider;
+    public S3Handler(final AmazonS3 s3Client) {
+        this.s3Client = s3Client;
     }
 
     @Override
-    public URLConnection openConnection(java.net.URL url) throws java.io.IOException {
-        return new S3URLConnection(url, credentialsProvider);
+    public URLConnection openConnection(final java.net.URL url) throws java.io.IOException {
+        return new S3URLConnection(url, s3Client);
     }
 }
