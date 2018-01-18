5751985545da2c053dcb2d9e61cdbe8ba70cd12c
Eric Trautman <trautmane@janelia.hhmi.org>
2018-01-18 16:15:44 -0500

    save alignmentQuality attribute for aligned tier stacks

--- render-ws-spark-client/src/main/java/org/janelia/render/client/spark/HierarchicalTierSolveFunction.java
+++ render-ws-spark-client/src/main/java/org/janelia/render/client/spark/HierarchicalTierSolveFunction.java
@@ -2,6 +2,7 @@ package org.janelia.render.client.spark;
 
 import java.io.File;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.broadcast.Broadcast;
@@ -9,6 +10,10 @@ import org.janelia.alignment.match.MatchCollectionId;
 import org.janelia.alignment.spec.Bounds;
 import org.janelia.alignment.spec.stack.HierarchicalStack;
 import org.janelia.alignment.spec.stack.StackId;
+import org.janelia.alignment.spec.stack.StackMetaData;
+import org.janelia.render.client.RenderDataClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Spark function for creating all stacks in a hierarchical tier.
@@ -57,9 +62,37 @@ public class HierarchicalTierSolveFunction
 
         // TODO: handle return code, how to handle missing matches? drill down one more tier? two empty tiers == stop?
 
-        // TODO: properly calculate alignment quality - hack here just saves return code (pass/fail)
-        tierStack.setAlignmentQuality((double) returnCode);
+        double tierStackAlignmentQuality = -1.0;
+
+        if (returnCode == 0) {
+            final StackId alignStackId = tierStack.getAlignedStackId();
+            final RenderDataClient localDataClient = new RenderDataClient(baseDataUrl,
+                                                                          alignStackId.getOwner(),
+                                                                          alignStackId.getProject());
+            final List<StackId> tierStacks = localDataClient.getProjectStacks();
+
+            if (tierStacks.contains(alignStackId)) {
+
+                final StackMetaData alignStackMetaData = localDataClient.getStackMetaData(alignStackId.getStack());
+                final Double alignmentQuality = alignStackMetaData.getCurrentVersion().getAlignmentQuality();
+                if (alignmentQuality != null) {
+                    tierStackAlignmentQuality = alignmentQuality;
+                } else {
+                    LOG.warn("alignment quality was not saved for {}", alignStackId);
+                }
+
+            } else {
+                LOG.warn("alignment stack {} does not exist", alignStackId);
+            }
+        }
+
+        LOG.info("setting alignment quality {} for {}", tierStackAlignmentQuality, tierStack.getSplitStackId());
+
+        tierStack.setAlignmentQuality(tierStackAlignmentQuality);
 
         return tierStack;
     }
+
+    private static final Logger LOG = LoggerFactory.getLogger(HierarchicalTierSolveFunction.class);
+
 }
