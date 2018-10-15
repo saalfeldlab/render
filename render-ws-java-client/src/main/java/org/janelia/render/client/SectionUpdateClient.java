33bfd8af82d6ab21a173c5c719cd5005fd05524b
Eric Trautman <trautmane@janelia.hhmi.org>
2018-10-15 12:22:24 -0400

    add box parameters to section update client

--- render-ws-java-client/src/main/java/org/janelia/render/client/SectionUpdateClient.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/SectionUpdateClient.java
@@ -3,6 +3,16 @@ package org.janelia.render.client;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParametersDelegate;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.janelia.alignment.spec.ResolvedTileSpecCollection;
+import org.janelia.alignment.spec.SectionData;
+import org.janelia.alignment.spec.TileBounds;
+import org.janelia.alignment.spec.TileBoundsRTree;
+import org.janelia.alignment.spec.TileSpec;
+import org.janelia.alignment.spec.stack.StackMetaData;
 import org.janelia.render.client.parameter.CommandLineParameters;
 import org.janelia.render.client.parameter.RenderWebServiceParameters;
 import org.slf4j.Logger;
@@ -28,6 +38,40 @@ public class SectionUpdateClient {
 
         @Parameter(names = "--z", description = "Z value", required = true)
         public Double z;
+
+        @Parameter(
+                names = "--completeToStackAfterUpdate",
+                description = "Complete the stack after updating z values",
+                arity = 0)
+        public boolean completeToStackAfterUpdate = false;
+
+        @Parameter(
+                names = "--minX",
+                description = "Minimum X value for all tiles to be updated"
+        )
+        public Double minX;
+
+        @Parameter(
+                names = "--minY",
+                description = "Minimum Y value for all tiles to be updated"
+        )
+        public Double minY;
+
+        @Parameter(
+                names = "--maxX",
+                description = "Maximum X value for all tiles to be updated"
+        )
+        public Double maxX;
+
+        @Parameter(
+                names = "--maxY",
+                description = "Maximum Y value for all tiles to be updated"
+        )
+        public Double maxY;
+
+        private boolean isBoxSpecified() {
+            return ((minX != null) && (minY != null) && (maxX != null) && (maxY != null));
+        }
     }
 
     /**
@@ -53,15 +97,70 @@ public class SectionUpdateClient {
     private final Parameters parameters;
     private final RenderDataClient renderDataClient;
 
-    public SectionUpdateClient(final Parameters parameters) {
+    private SectionUpdateClient(final Parameters parameters) {
         this.parameters = parameters;
         this.renderDataClient = parameters.renderWeb.getDataClient();
     }
 
-    public void updateZ()
+    private void updateZ()
             throws Exception {
-        renderDataClient.ensureStackIsInLoadingState(parameters.stack, null);
-        renderDataClient.updateZForSection(parameters.stack, parameters.sectionId, parameters.z);
+
+        final String stack = parameters.stack;
+        final String sectionId = parameters.sectionId;
+
+        renderDataClient.ensureStackIsInLoadingState(stack, null);
+
+        if (parameters.isBoxSpecified()) {
+
+            final List<SectionData> sectionDataList = renderDataClient.getStackSectionData(stack, null, null);
+
+            Double zForSectionId = null;
+            for (final SectionData sectionData : sectionDataList) {
+                if (sectionId.equals(sectionData.getSectionId())) {
+                    zForSectionId = sectionData.getZ();
+                    break;
+                }
+            }
+
+            if (zForSectionId == null) {
+                throw new IllegalArgumentException("sectionId '" + sectionId + "' not found in stack " + stack);
+            }
+
+            final List<TileBounds> tileBoundsList = renderDataClient.getTileBounds(stack, zForSectionId);
+            final TileBoundsRTree tree = new TileBoundsRTree(zForSectionId, tileBoundsList);
+            final Set<String> updateTileIds = new HashSet<>(tileBoundsList.size() * 2);
+            for (final TileBounds tileBounds : tree.findTilesInBox(parameters.minX, parameters.minY,
+                                                                   parameters.maxX, parameters.maxY)) {
+                if (sectionId.equals(tileBounds.getSectionId())) {
+                    updateTileIds.add(tileBounds.getTileId());
+                }
+            }
+
+            if (updateTileIds.size() == 0) {
+                throw new IllegalArgumentException("no tile specs exist for stack " + stack + " in the bounding box: " +
+                                                   "min (" + parameters.minX + ", " + parameters.minY +
+                                                   "), max (" + parameters.maxX + ", " + parameters.maxY  + ")");
+            }
+
+            final ResolvedTileSpecCollection resolvedTiles = renderDataClient.getResolvedTiles(stack, zForSectionId);
+
+            resolvedTiles.removeDifferentTileSpecs(updateTileIds);
+
+
+            for (final TileSpec tileSpec : resolvedTiles.getTileSpecs()) {
+                tileSpec.setZ(parameters.z);
+            }
+
+            renderDataClient.saveResolvedTiles(resolvedTiles, stack, null);
+
+        } else {
+            renderDataClient.updateZForSection(stack, parameters.sectionId, parameters.z);
+        }
+
+        if (parameters.completeToStackAfterUpdate) {
+            renderDataClient.setStackState(stack, StackMetaData.StackState.COMPLETE);
+        }
+
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(SectionUpdateClient.class);
