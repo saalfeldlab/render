38bcacd5039f126b7d0e28a19d932599f916d2f9
Stephan Preibisch <stephan.preibisch@gmx.de>
2020-01-12 18:26:51 -0500

    fixed lambda's (rigid to affine) split into abstract class and simple implementation that we keep ... doing the fully blocked implementation now

--- render-ws-java-client/src/test/java/org/janelia/render/client/PartialSolve.java
+++ render-ws-java-client/src/test/java/org/janelia/render/client/PartialSolve.java
@@ -3,25 +3,16 @@ package org.janelia.render.client;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.janelia.alignment.RenderParameters;
 import org.janelia.alignment.Renderer;
-import org.janelia.alignment.match.CanvasId;
-import org.janelia.alignment.match.CanvasMatchResult;
-import org.janelia.alignment.match.CanvasMatches;
-import org.janelia.alignment.match.Matches;
 import org.janelia.alignment.match.ModelType;
-import org.janelia.alignment.match.MontageRelativePosition;
 import org.janelia.alignment.spec.Bounds;
 import org.janelia.alignment.spec.ReferenceTransformSpec;
 import org.janelia.alignment.spec.ResolvedTileSpecCollection;
@@ -41,7 +32,6 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParametersDelegate;
 
 import ij.IJ;
-import ij.ImageJ;
 import ij.ImagePlus;
 import ij.io.FileSaver;
 import ij.measure.Calibration;
@@ -49,367 +39,35 @@ import mpicbg.models.Affine2D;
 import mpicbg.models.AffineModel2D;
 import mpicbg.models.CoordinateTransform;
 import mpicbg.models.CoordinateTransformList;
-import mpicbg.models.ErrorStatistic;
 import mpicbg.models.InterpolatedAffineModel2D;
 import mpicbg.models.Model;
 import mpicbg.models.NoninvertibleModelException;
-import mpicbg.models.Point;
-import mpicbg.models.PointMatch;
 import mpicbg.models.Tile;
-import mpicbg.models.TileConfiguration;
-import mpicbg.models.TileUtil;
 import mpicbg.trakem2.transform.TransformMeshMappingWithMasks.ImageProcessorWithMasks;
 import net.imglib2.Cursor;
 import net.imglib2.IterableInterval;
-import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.multithreading.SimpleMultiThreading;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Pair;
-import net.imglib2.util.Util;
 import net.imglib2.util.ValuePair;
 import net.imglib2.view.Views;
 
-public class DistributedSolve< B extends Model< B > & Affine2D< B > >
+public abstract class PartialSolve< B extends Model< B > & Affine2D< B > >
 {
-	final Parameters parameters;
-	private final RenderDataClient renderDataClient;
-	private final RenderDataClient matchDataClient;
-	private final RenderDataClient targetDataClient;
+	final protected Parameters parameters;
+	protected final RenderDataClient renderDataClient;
+	protected final RenderDataClient matchDataClient;
+	protected final RenderDataClient targetDataClient;
 	
-	private final List<String> pGroupList;
-	private final Map<String, List<Double>> sectionIdToZMap;
-	private final Map<Double, ResolvedTileSpecCollection> zToTileSpecsMap;
-	private double minZ, maxZ; // min will be fixed, max will be grouped
-	private int totalTileCount;
-
-	private void run() throws IOException, ExecutionException, InterruptedException, NoninvertibleModelException
-	{
-		LOG.info("run: entry");
-
-		LOG.info( "fixing tiles from layer " + minZ );
-		LOG.info( "grouping tiles from layer " + maxZ );
-
-		final HashMap<String, Tile<InterpolatedAffineModel2D<AffineModel2D, B>>> idToTileMap = new HashMap<>();
-		final HashMap<String, AffineModel2D> idToPreviousModel = new HashMap<>();
-		final HashMap<String, TileSpec> idToTileSpec = new HashMap<>();
-
-		final List< Tile<InterpolatedAffineModel2D<AffineModel2D, B>> > fixedTiles = new ArrayList<>();
-		final List< String > fixedTileNames = new ArrayList<>();
-
-		// we want group all tiles of the last layer to have a common transformation that we can propgate through the rest of the stack
-		// therefore, we also want a differential transform for the grouped tiles, which means that we have to apply the current 
-		// transformation to the local points
-		final HashSet< String > groupedTiles = new HashSet<>(); 
-		final Tile<InterpolatedAffineModel2D<AffineModel2D, B>> groupedTile = new Tile<>(
-				new InterpolatedAffineModel2D<>(
-					new AffineModel2D(),
-					parameters.regularizerModelType.getInstance(),
-					parameters.startLambda)); // note: lambda gets reset during optimization loops
-
-		ArrayList< String > log = new ArrayList<>();
-
-		for (final String pGroupId : pGroupList)
-		{
-			LOG.info("run: connecting tiles with pGroupId {}", pGroupId);
-
-			final List<CanvasMatches> matches = matchDataClient.getMatchesWithPGroupId(pGroupId, false);
-
-			for (final CanvasMatches match : matches)
-			{
-				final String pId = match.getpId();
-				final TileSpec pTileSpec = getTileSpec(pGroupId, pId);
-
-				final String qGroupId = match.getqGroupId();
-				final String qId = match.getqId();
-				final TileSpec qTileSpec = getTileSpec(qGroupId, qId);
-
-				if ((pTileSpec == null) || (qTileSpec == null))
-				{
-					LOG.info("run: ignoring pair ({}, {}) because one or both tiles are missing from stack {}", pId, qId, parameters.stack);
-					continue;
-				}
-
-				boolean pGrouped = false;
-				boolean qGrouped = false;
-
-				final Tile<InterpolatedAffineModel2D<AffineModel2D, B>> p, q;
-
-				if ( !idToTileMap.containsKey( pId ) )
-				{
-					// isgrouped?
-					if ( Math.round( pTileSpec.getZ() ) == Math.round( maxZ ) )
-					{
-						pGrouped = true;
-						groupedTiles.add( pId );
-						log.add( "Adding " + pId + "(grouped=" + pGrouped + ") to grouped" );
-
-						p = groupedTile;
-						idToTileMap.put( pId, groupedTile );
-						idToPreviousModel.put( pId, loadLastTransformFromSpec( pTileSpec ).copy() );
-						idToTileSpec.put( pId, pTileSpec );
-					}
-					else
-					{
-						pGrouped = false;
-						final Pair< Tile<InterpolatedAffineModel2D<AffineModel2D, B>>, AffineModel2D > pairP = buildTileFromSpec(pTileSpec);
-						p = pairP.getA();
-						idToTileMap.put( pId, p );
-						idToPreviousModel.put( pId, pairP.getB() );
-						idToTileSpec.put( pId, pTileSpec );
-					}
-
-					// isfixed?
-					if ( Math.round( pTileSpec.getZ() ) == Math.round( minZ ) )
-					{
-						fixedTiles.add( p );
-						fixedTileNames.add( pId );
-					}
-				}
-				else
-				{
-					p = idToTileMap.get( pId );
-
-					if ( groupedTiles.contains( pId ))
-						pGrouped = true;
-					else
-						pGrouped = false;
-				}
-
-				if ( !idToTileMap.containsKey( qId ) )
-				{
-					// isgrouped?
-					if ( Math.round( qTileSpec.getZ() ) == Math.round( maxZ ) )
-					{
-						qGrouped = true;
-						groupedTiles.add( qId );
-						log.add( "Adding " + qId + "(grouped=" + qGrouped + ") to grouped" );
-
-						q = groupedTile;
-						idToTileMap.put( qId, groupedTile );
-						idToPreviousModel.put( qId, loadLastTransformFromSpec( qTileSpec ).copy() );
-						idToTileSpec.put( qId, qTileSpec );
-					}
-					else
-					{
-						qGrouped = false;
-						final Pair< Tile<InterpolatedAffineModel2D<AffineModel2D, B>>, AffineModel2D > pairQ = buildTileFromSpec(qTileSpec);
-						q = pairQ.getA();
-						idToTileMap.put( qId, q );
-						idToPreviousModel.put( qId, pairQ.getB() );
-						idToTileSpec.put( qId, qTileSpec );	
-					}
-
-					// isfixed?
-					if ( Math.round( qTileSpec.getZ() ) == Math.round( minZ ) )
-					{
-						fixedTiles.add( q );
-						fixedTileNames.add( qId );
-					}
-				}
-				else
-				{
-					q = idToTileMap.get( qId );
-
-					if ( groupedTiles.contains( qId ))
-						qGrouped = true;
-					else
-						qGrouped = false;
-				}
-
-				// both images are not grouped, this is the "classical" case
-				if ( !pGrouped && !qGrouped )
-				{
-					p.connect(q, CanvasMatchResult.convertMatchesToPointMatchList(match.getMatches()));
-				}
-				else if ( pGrouped && qGrouped )
-				{
-					// both images are part of the same grouped tile, nothing to do
-					log.add( "Ignoring matches between " + pId + "(grouped=" + pGrouped + ") <> " + qId + "(grouped=" + qGrouped + ")" );
-				}
-				else // either p or q is grouped
-				{
-					// as we cannot build the grouped Tile yet, we need to remember all connections to it
-					log.add( "Putting grouped matches between " + pId + "(grouped=" + pGrouped + ") <> " + qId + "(grouped=" + qGrouped + ")" );
-
-					final AffineModel2D pModel = pGrouped ? idToPreviousModel.get( pId ) : null;
-					final AffineModel2D qModel = qGrouped ? idToPreviousModel.get( qId ) : null;
-
-					log.add( "pModel: " + pModel + " qModel: " + qModel  );
-
-					p.connect(q, convertMatchesToPointMatchListRelative(match.getMatches(), pModel, qModel ));
-				}
-
-			}
-		}
-
-		LOG.info( "Fixed tiles:" );
-
-		for ( int i = 0; i < fixedTiles.size(); ++i )
-			LOG.info( fixedTileNames.get( i ) + " " + fixedTiles.get( i ) + " model: " + idToPreviousModel.get( fixedTileNames.get( i ) ) );
-
-		LOG.info( "Grouped tiles:" );
-
-		for ( final String tile : groupedTiles )
-			LOG.info( tile );
-
-		LOG.info( "Grouped matches:" );
-
-		for ( final String tile : log )
-			LOG.info( tile );
-
-		//System.exit( 0 );
-
-		final TileConfiguration tileConfig = new TileConfiguration();
-		tileConfig.addTiles(idToTileMap.values());
-
-		for ( final Tile<InterpolatedAffineModel2D<AffineModel2D, B>> t : fixedTiles )
-			tileConfig.fixTile( t );
-		
-		LOG.info("run: optimizing {} tiles", idToTileMap.size());
-
-		final List<Double> lambdaValues = new ArrayList<>();
-		lambdaValues.add( 0.0 );
-		lambdaValues.add( 0.1 );
-		lambdaValues.add( 0.5 );
-
-		/*
-		if (parameters.optimizerLambdas == null) {
-			lambdaValues = Stream.of(1.0, 0.5, 0.1, 0.01)
-					.filter(lambda -> lambda <= parameters.startLambda)
-					.collect(Collectors.toList());
-		} else {
-			lambdaValues = parameters.optimizerLambdas.stream()
-					.sorted(Comparator.reverseOrder())
-					.collect(Collectors.toList());
-		}
-		*/
-
-		for (final double lambda : lambdaValues)
-		{
-			for (final Tile tile : idToTileMap.values()) {
-				((InterpolatedAffineModel2D) tile.getModel()).setLambda(lambda);
-			}
-
-			final int numIterations = lambda > 0.25 ? 200 : parameters.maxIterations;
-
-			// tileConfig.optimize(parameters.maxAllowedError, parameters.maxIterations, parameters.maxPlateauWidth);
-		
-			final ErrorStatistic observer = new ErrorStatistic(parameters.maxPlateauWidth + 1 );
-			final float damp = 1.0f;
-			TileUtil.optimizeConcurrently(
-					observer,
-					parameters.maxAllowedError,
-					numIterations,
-					parameters.maxPlateauWidth,
-					damp,
-					tileConfig,
-					tileConfig.getTiles(),
-					tileConfig.getFixedTiles(),
-					parameters.numberOfThreads);
-		}
-
-		if (parameters.targetStack == null)
-		{
-			final HashMap<String, AffineModel2D> idToNewModel = new HashMap<>();
-
-			AffineModel2D propagationModel = null;
-
-			final ArrayList< String > tileIds = new ArrayList<>( idToTileMap.keySet() );
-			Collections.sort( tileIds );
-
-			for (final String tileId : tileIds )
-			{
-				final Tile<InterpolatedAffineModel2D<AffineModel2D, B>> tile = idToTileMap.get(tileId);
-				final InterpolatedAffineModel2D model = tile.getModel();
-				AffineModel2D affine = model.createAffineModel2D();
-
-				if ( groupedTiles.contains( tileId ) )
-				{
-					// the relative propagation model of the last z plane
-					if ( propagationModel == null && idToTileSpec.get( tileId ).getZ() == maxZ && groupedTiles.contains( tileId ) )
-						propagationModel = affine.copy();
-
-					final AffineModel2D previous = idToPreviousModel.get( tileId ).copy();
-
-					LOG.info( "tile {} model is grouped and therefore a relative model {}", tileId, affine);
-					LOG.info( "concatenating with previous model {} ", previous );
-
-					previous.preConcatenate( affine );
-					affine = previous;
-				}
-
-				idToNewModel.put( tileId, affine );
-				LOG.info("tile {} model is {}", tileId, affine);
-			}
-
-			LOG.info("Relative propagation model for all layers > " + maxZ + " = " + propagationModel );
-
-			new ImageJ();
-
-			// visualize new result
-			render( idToNewModel, idToTileSpec, 0.15 );
-
-			// visualize old result
-			//render( idToPreviousModel, idToTileSpec, 0.15 );
-
-			SimpleMultiThreading.threadHaltUnClean();
-		}
-		else
-		{
-			//saveTargetStackTiles(idToTileMap);
-		}
-
-
-		LOG.info("run: exit");
-	}
-
-	/**
-	 * @param  matches  point match list in {@link Matches} form.
-     * @param  pModel  affine model that will transform the p points (can be used to extract relative models from the optimization) or will do nothing if pModel == null
-	 * @param  qModel  affine model that will transform the q points (can be used to extract relative models from the optimization) or will do nothing if qModel == null
-     *
-     * @return the corresponding list of {@link PointMatch} objects.
-	 */
-	public static List< PointMatch > convertMatchesToPointMatchListRelative( final Matches matches, final AffineModel2D pModel, final AffineModel2D qModel )
-	{
-		final double[] w = matches.getWs();
-
-		final int pointMatchCount = w.length;
-		final List< PointMatch > pointMatchList = new ArrayList<>( pointMatchCount );
-
-		if ( pointMatchCount > 0 )
-		{
-			final double[][] p = matches.getPs();
-			final double[][] q = matches.getQs();
-
-			final int dimensionCount = p.length;
-
-			for (int matchIndex = 0; matchIndex < pointMatchCount; matchIndex++)
-			{
-				final double[] pLocal = new double[ dimensionCount ];
-				final double[] qLocal = new double[ dimensionCount ];
-
-				for (int dimensionIndex = 0; dimensionIndex < dimensionCount; dimensionIndex++)
-				{
-					pLocal[ dimensionIndex ] = p[ dimensionIndex ][ matchIndex ];
-					qLocal[ dimensionIndex ] = q[ dimensionIndex ][ matchIndex ];
-				}
-
-				if ( pModel != null )
-					pModel.applyInPlace( pLocal );
-
-				if ( qModel != null )
-					qModel.applyInPlace( qLocal );
-
-				pointMatchList.add( new PointMatch( new Point( pLocal ), new Point( qLocal ), w[ matchIndex ] ) );
-			}
-		}
-
-		return pointMatchList;
-	}
+	protected final List<String> pGroupList;
+	protected final Map<String, List<Double>> sectionIdToZMap;
+	protected final Map<Double, ResolvedTileSpecCollection> zToTileSpecsMap;
+	protected double minZ, maxZ; // min will be fixed, max will be grouped
+	protected int totalTileCount;
+
+	protected abstract void run() throws IOException, ExecutionException, InterruptedException, NoninvertibleModelException;
 
 	public void render( final HashMap<String, AffineModel2D> models, final HashMap<String, TileSpec> idToTileSpec, final double scale ) throws NoninvertibleModelException
 	{
@@ -531,7 +189,7 @@ public class DistributedSolve< B extends Model< B > & Affine2D< B > >
 		imp.show();
 	}
 
-	public DistributedSolve(final Parameters parameters) throws IOException
+	public PartialSolve(final Parameters parameters) throws IOException
 	{
 		parameters.initDefaultValues();
 
@@ -617,7 +275,7 @@ public class DistributedSolve< B extends Model< B > & Affine2D< B > >
 		});
 	}
 
-	private ImageProcessorWithMasks getImage( final TileSpec tileSpec, final double scale )
+	protected ImageProcessorWithMasks getImage( final TileSpec tileSpec, final double scale )
 	{
 		final File imageFile = new File( "tmp", tileSpec.getTileId() + "_" + scale + ".image.tif" );
 		final File maskFile = new File( "tmp", tileSpec.getTileId() + "_" + scale + ".mask.tif" );
@@ -663,7 +321,7 @@ public class DistributedSolve< B extends Model< B > & Affine2D< B > >
 		return imp;
 	}
 
-	private AffineModel2D loadLastTransformFromSpec( final TileSpec tileSpec )
+	protected AffineModel2D loadLastTransformFromSpec( final TileSpec tileSpec )
 	{
 		// TODO: make sure there is only one transform
         final CoordinateTransformList<CoordinateTransform> transformList = tileSpec.getTransformList();
@@ -672,7 +330,7 @@ public class DistributedSolve< B extends Model< B > & Affine2D< B > >
         return lastTransform;
 	}
 
-	private Pair< Tile<InterpolatedAffineModel2D<AffineModel2D, B>>, AffineModel2D > buildTileFromSpec( final TileSpec tileSpec )
+	protected Pair< Tile<InterpolatedAffineModel2D<AffineModel2D, B>>, AffineModel2D > buildTileFromSpec( final TileSpec tileSpec )
 	{
         final AffineModel2D lastTransform = loadLastTransformFromSpec( tileSpec );
         final AffineModel2D lastTransformCopy = lastTransform.copy();
@@ -698,7 +356,7 @@ public class DistributedSolve< B extends Model< B > & Affine2D< B > >
         		lastTransform.copy() );
 	}
 
-    private TileSpec getTileSpec(final String sectionId,
+	protected TileSpec getTileSpec(final String sectionId,
                                  final String tileId)
             throws IOException {
 
@@ -768,49 +426,6 @@ public class DistributedSolve< B extends Model< B > & Affine2D< B > >
 		return renderParameters;
 	}
 
-	public static void main( String[] args )
-	{
-        final ClientRunner clientRunner = new ClientRunner(args) {
-            @Override
-            public void runClient(final String[] args) throws Exception {
-
-                final Parameters parameters = new Parameters();
-
-                // TODO: remove testing hack ...
-                if (args.length == 0) {
-                    final String[] testArgs = {
-                            "--baseDataUrl", "http://renderer-dev.int.janelia.org:8080/render-ws/v1",
-                            "--owner", "Z1217_19m",
-                            "--project", "Sec10",
-
-                            "--stack", "v2_patch_msolve_fine",
-                            //"--targetStack", "null",
-                            "--regularizerModelType", "RIGID",
-                            "--optimizerLambdas", "1.0",
-                            "--minZ", "20500",
-                            "--maxZ", "20600",
-
-                            "--threads", "4",
-                            "--maxIterations", "10000",
-                            "--completeTargetStack",
-                            "--matchCollection", "Sec10_patch"
-                    };
-                    parameters.parse(testArgs);
-                } else {
-                    parameters.parse(args);
-                }
-
-                LOG.info("runClient: entry, parameters={}", parameters);
-
-                final DistributedSolve client = new DistributedSolve(parameters);
-
-                client.run();
-            }
-        };
-        clientRunner.run();
-	}
-
-	
     public static class Parameters extends CommandLineParameters {
 
 		private static final long serialVersionUID = 6845718387096692785L;
@@ -946,5 +561,5 @@ public class DistributedSolve< B extends Model< B > & Affine2D< B > >
 
     }
 
-	private static final Logger LOG = LoggerFactory.getLogger(DistributedSolve.class);
+	private static final Logger LOG = LoggerFactory.getLogger(PartialSolve.class);
 }
