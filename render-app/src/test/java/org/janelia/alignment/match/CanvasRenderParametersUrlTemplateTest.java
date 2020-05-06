e30a5bb5ee5f3cf606bee074e31d53006f4190a8
Eric Trautman <trautmane@janelia.hhmi.org>
2020-05-05 21:38:47 -0400

    move url template tests to proper class

--- render-app/src/test/java/org/janelia/alignment/match/CanvasRenderParametersUrlTemplateTest.java
+++ render-app/src/test/java/org/janelia/alignment/match/CanvasRenderParametersUrlTemplateTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
  */
 public class CanvasRenderParametersUrlTemplateTest {
 
+    private String tileId = "aaa";
+
     @Test
     public void testLoad() throws Exception {
 
@@ -48,6 +50,34 @@ public class CanvasRenderParametersUrlTemplateTest {
                             baseDataUrl + "/owner/flyTEM/project/FAFB00/stack/v12_acquire_merged/tile/{id}/render-parameters?" +
                             "width=2760&height=2330&scale=0.8&filter=true&filterListName=fav&excludeMask=true&normalizeForMatching=true",
                             templateForRun.getTemplateString());
+    }
+
+    @Test
+    public void testGetRenderParametersUrl() {
+
+
+        testTemplate("http://render:8080/render-ws/v1/tile/{id}/render-parameters",
+                     "http://render:8080/render-ws/v1/tile/aaa/render-parameters");
+
+        testTemplate("http://render:8080/render-ws/v1/z/{groupId}/render-parameters",
+                     "http://render:8080/render-ws/v1/z/99.0/render-parameters");
+
+        testTemplate("http://render:8080/render-ws/v1/z/{groupId}/tile/{id}/render-parameters",
+                     "http://render:8080/render-ws/v1/z/99.0/tile/aaa/render-parameters");
+
+        tileId = "z_1.0_box_12769_7558_13654_18227_0.1";
+        testTemplate("http://render:8080/render-ws/v1/z/{groupId}/box/{id}/render-parameters",
+                     "http://render:8080/render-ws/v1/z/99.0/box/12769,7558,13654,18227,0.1/render-parameters");
+    }
+
+    private void testTemplate(final String templateString,
+                              final String expectedResult) {
+
+        final CanvasRenderParametersUrlTemplate template = new CanvasRenderParametersUrlTemplate(templateString);
+        final CanvasId canvasId = new CanvasId("99.0", tileId);
+        Assert.assertEquals("failed to parse template " + template,
+                            expectedResult,
+                            template.getRenderParametersUrl(canvasId));
     }
 
 }
