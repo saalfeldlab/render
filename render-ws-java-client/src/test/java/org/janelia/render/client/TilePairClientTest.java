f4e3f8696130a7f9ddde609ac5cfb9e31442367c
Eric Trautman <trautmane@janelia.hhmi.org>
2020-08-07 21:29:05 -0400

    changed junit.framework.Assert references to org.junit.Assert

--- render-ws-java-client/src/test/java/org/janelia/render/client/TilePairClientTest.java
+++ render-ws-java-client/src/test/java/org/janelia/render/client/TilePairClientTest.java
@@ -24,7 +24,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 /**
  * Tests the {@link TilePairClient} class.
