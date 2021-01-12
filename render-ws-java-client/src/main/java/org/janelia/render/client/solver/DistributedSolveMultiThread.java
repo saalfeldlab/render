2beaacc6c6c3b6462dfb1b2f7314b04a7d31b7fc
Stephan Preibisch <preibischs@janelia.hhmi.org>
2021-01-12 16:49:35 -0500

    change solve paramaters

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/DistributedSolveMultiThread.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/DistributedSolveMultiThread.java
@@ -115,15 +115,15 @@ public class DistributedSolveMultiThread extends DistributedSolve
                             "--project", "Z0419_25_Alpha3", //"jrc_hela_2", //"Sec10",
                             "--matchCollection", "Z0419_25_Alpha3_v1", //"jrc_hela_2_v1", //"Sec10_multi",
                             "--stack", "v1_acquire", //"v3_acquire",
-                            "--targetStack", "v1_acquire_sp",
+                            "--targetStack", "v1_acquire_sp_translation_nodyn",
                             "--completeTargetStack",
                             
                             //"--noreg","400, 23434, 23-254",
                             
-                            "--blockOptimizerLambdasRigid",       /*"1.0,1.0,0.5,0.1,0.01",*/"1.0,1.0,1.0",
-                            "--blockOptimizerLambdasTranslation", /*"0.5,0.0,0.0,0.0,0.0",*/"0.0,0.0,0.0",
-                            "--blockOptimizerIterations", "1000,1000,500",
-                            "--blockMaxPlateauWidth", "250,250,150",
+                            "--blockOptimizerLambdasRigid",       "1.0", //"1.0,1.0,0.9,0.1,0.01",
+                            "--blockOptimizerLambdasTranslation", "1.0", //"1.0,0.5,0.0,0.0,0.0",
+                            "--blockOptimizerIterations", "2000", //"1000,1000,500,250,250",
+                            "--blockMaxPlateauWidth", "500",//"250,250,150,100,100",
 
                             //"--blockSize", "100",
                             //"--noStitching", // do not stitch first
@@ -133,10 +133,12 @@ public class DistributedSolveMultiThread extends DistributedSolve
 
                             "--maxNumMatches", "0", // no limit, default
                             "--threadsWorker", "1", 
-                            "--threadsGlobal", "8",
+                            "--threadsGlobal", "72",
                             "--maxPlateauWidthGlobal", "50",
                             "--maxIterationsGlobal", "10000",
-							"--serializerDirectory", "."
+							"--serializerDirectory", "./ser-translation_nodyn",
+							"--serializeMatches",
+							"--dynamicLambdaFactor", "0.00"
                     };
                     parameters.parse(testArgs);
                 } else {
