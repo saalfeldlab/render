1f0cde80af5dea805c7b9caa5e29b8c7fc0a189c
Stephan Preibisch <preibischs@janelia.hhmi.org>
2021-01-13 13:55:02 -0500

    fixes for global min and optimizations and catching npes

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/visualize/lazy/RenderRA.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/visualize/lazy/RenderRA.java
@@ -30,6 +30,7 @@ import org.janelia.render.client.solver.visualize.RenderTools;
 
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
+import mpicbg.trakem2.transform.TransformMeshMappingWithMasks.ImageProcessorWithMasks;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.exception.IncompatibleTypeException;
@@ -55,6 +56,7 @@ public class RenderRA<T extends RealType<T> & NativeType<T>> implements Consumer
 	final String project;// = "Z0419_25_Alpha3";
 	final String stack;// = "v1_acquire_sp_translation_nodyn";
 
+	final long minZ, maxZ;
 	final double scale;
 
 	public RenderRA(
@@ -62,6 +64,8 @@ public class RenderRA<T extends RealType<T> & NativeType<T>> implements Consumer
 			final String owner,
 			final String project,
 			final String stack,
+			final long minZ, // full-res stack
+			final long maxZ, // full-res stack
 			final ImageProcessorCache ipCache,
 			final long[] min,
 			final T type,
@@ -75,6 +79,8 @@ public class RenderRA<T extends RealType<T> & NativeType<T>> implements Consumer
 		this.globalMin = min;
 		this.type = type;
 		this.scale = scale;
+		this.minZ = minZ;
+		this.maxZ = maxZ;
 	}
 
 	// Note: the output RAI typically sits at 0,0...0 because it usually is a CachedCellImage
@@ -91,8 +97,8 @@ public class RenderRA<T extends RealType<T> & NativeType<T>> implements Consumer
 			for ( int d = 0; d < min.length; ++d )
 				min[ d ] = globalMin[ d ] + output.min( d );
 
-			final int x = (int)Math.round( output.min( 0 ) / scale );
-			final int y = (int)Math.round( output.min( 1 ) / scale );
+			final int x = (int)Math.round( min[ 0 ] / scale );
+			final int y = (int)Math.round( min[ 1 ] / scale );
 
 			final int w = (int)Math.round( output.dimension( 0 ) / scale );
 			final int h = (int)Math.round( output.dimension( 1 ) / scale );
@@ -100,21 +106,42 @@ public class RenderRA<T extends RealType<T> & NativeType<T>> implements Consumer
 			// we assume that the blocksize in z == 1
 			final int z = (int)Math.round( output.min( 2 ) / scale );
 
-			ImageProcessor ip = RenderTools.renderImage(ipCache, baseUrl, owner, project, stack, x, y, z, w, h, z, false ).ip;
-			RandomAccessibleInterval img;
+			if ( z < minZ || z > maxZ )
+			{
+				fillZero( output );
+				return;
+			}
+			else
+			{
+				ImageProcessorWithMasks ipm = RenderTools.renderImage( ipCache, baseUrl, owner, project, stack, x, y, z, w, h, scale, false );
 
-			img = ImagePlusImgs.from( new ImagePlus( "", ip ) );
-			img = Views.translate( img, min );
+				if ( ipm == null ) // if the requested block contains no images, null will be returned
+				{
+					fillZero( output );
+					return;
+				}
 
-			final Cursor< T > out = Views.flatIterable( output ).cursor();
-			final Cursor< RealType > in = Views.flatIterable( img ).cursor();
+				final Cursor< T > out = Views.flatIterable( output ).cursor();
+				int i = 0;
 
-			while( out.hasNext() )
-				out.next().setReal( in.next().getRealDouble() );
+				while( out.hasNext() )
+					out.next().setReal( ipm.ip.getf( i++ ) );
+			}
 		}
 		catch (final IncompatibleTypeException e)
 		{
 			throw new RuntimeException(e);
 		}
+	}
+
+	protected static final void update()
+	{
+		
+	}
+
+	protected void fillZero( final RandomAccessibleInterval<T> output )
+	{
+		for ( final T t : Views.iterable( output ) )
+			t.setZero();
 	}
 }
