c83c292bda7bfe08ed2e462c48a17ac53806c6f8
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-01-13 06:16:42 -0500

    computing the bounding box is now a static method

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/visualize/VisualizingRandomAccessibleInterval.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/visualize/VisualizingRandomAccessibleInterval.java
@@ -2,6 +2,7 @@ package org.janelia.render.client.solver.visualize;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.janelia.render.client.solver.MinimalTileSpec;
 
@@ -35,68 +36,9 @@ public class VisualizingRandomAccessibleInterval implements RandomAccessibleInte
 		this.scale = scale;
 		this.idToValue = idToValue;
 
-		final double[] min = new double[] { Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE };
-		final double[] max = new double[] { -Double.MAX_VALUE, -Double.MAX_VALUE, -Double.MAX_VALUE };
-
-		final double[] tmpMin = new double[ 2 ];
-		final double[] tmpMax = new double[ 2 ];
-
-		final AffineModel2D scaleModel = new AffineModel2D();
-		scaleModel.set( scale[ 0 ], 0, 0, scale[ 1 ], 0, 0 );
-
 		this.idToInvertedRenderModels = new HashMap<>();
 
-		// get bounding box
-		for ( final String tileId : idToModels.keySet() )
-		{
-			final MinimalTileSpec tileSpec = idToTileSpec.get( tileId );
-			min[ 2 ] = Math.min( min[ 2 ], tileSpec.getZ() * scale[ 2 ] );
-			max[ 2 ] = Math.max( max[ 2 ], tileSpec.getZ() * scale[ 2 ] );
-
-			final int w = tileSpec.getWidth();
-			final int h = tileSpec.getHeight();
-
-			final AffineModel2D model = idToModels.get( tileId ).copy();
-
-			// scale the actual transform down to the scale level we want to render in
-			model.preConcatenate( scaleModel );
-
-			tmpMin[ 0 ] = 0;
-			tmpMin[ 1 ] = 0;
-			tmpMax[ 0 ] = w - 1;
-			tmpMax[ 1 ] = h - 1;
-
-			model.estimateBounds( tmpMin, tmpMax );
-
-			min[ 0 ] = Math.min( min[ 0 ], Math.min( tmpMin[ 0 ], tmpMax[ 0 ] ) );
-			max[ 0 ] = Math.max( max[ 0 ], Math.max( tmpMin[ 0 ], tmpMax[ 0 ] ) );
-
-			min[ 1 ] = Math.min( min[ 1 ], Math.min( tmpMin[ 1 ], tmpMax[ 1 ] ) );
-			max[ 1 ] = Math.max( max[ 1 ], Math.max( tmpMin[ 1 ], tmpMax[ 1 ] ) );
-
-			idToInvertedRenderModels.put( tileId, model.createInverse() );
-		}
-
-		//System.out.println( "x: " + min[ 0 ] + " >>> " + max[ 0 ] );
-		//System.out.println( "y: " + min[ 1 ] + " >>> " + max[ 1 ] );
-		//System.out.println( "z: " + min[ 2 ] + " >>> " + max[ 2 ] );
-
-		final long[] minI = new long[ 3 ];
-		final long[] maxI = new long[ 3 ];
-		final long[] dimI = new long[ 3 ];
-
-		for ( int d = 0; d < minI.length; ++d )
-		{
-			minI[ d ] = Math.round( Math.floor( min[ d ] ) );
-			maxI[ d ] = Math.round( Math.ceil( max[ d ] ) );
-			dimI[ d ] = maxI[ d ] - minI[ d ] + 1;
-		}
-
-		//System.out.println( "BB x: " + minI[ 0 ] + " >>> " + maxI[ 0 ] + ", d=" + dimI[ 0 ] );
-		//System.out.println( "BB y: " + minI[ 1 ] + " >>> " + maxI[ 1 ] + ", d=" + dimI[ 1 ]);
-		//System.out.println( "BB z: " + minI[ 2 ] + " >>> " + maxI[ 2 ] + ", d=" + dimI[ 2 ]);
-
-		this.interval = new FinalInterval( minI, maxI );
+		this.interval = computeInterval( idToModels, idToTileSpec, idToInvertedRenderModels, scale );
 
 		// build the lookup z to tilespec
 		this.zToTileSpec = new HashMap<>(); 
@@ -166,4 +108,96 @@ public class VisualizingRandomAccessibleInterval implements RandomAccessibleInte
 
 	@Override
 	public long dimension( final int d ) { return interval.dimension( d ); }
+
+	/**
+	 * @param idToModels - map from all tileIds to their models
+	 * @param idToTileSpec - map from all tileIds to their TileSpec
+	 * @param scale - the 3d scaling
+	 * 
+	 * @return the bounding box of all rendered tiles
+	 */
+	public static Interval computeInterval(
+			final Map<String, AffineModel2D> idToModels,
+			final Map<String, MinimalTileSpec> idToTileSpec,
+			final double[] scale )
+	{
+		return computeInterval( idToModels, idToTileSpec, null, scale );
+	}
+
+	/**
+	 * @param idToModels - map from all tileIds to their models
+	 * @param idToTileSpec - map from all tileIds to their TileSpec
+	 * @param idToInvertedRenderModels - will be populated if not null
+	 * @param scale - the 3d scaling
+	 * 
+	 * @return the bounding box of all rendered tiles
+	 */
+	public static Interval computeInterval(
+			final Map<String, AffineModel2D> idToModels,
+			final Map<String, MinimalTileSpec> idToTileSpec,
+			final Map<String, AffineModel2D> idToInvertedRenderModels,
+			final double[] scale )
+	{
+		final double[] min = new double[] { Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE };
+		final double[] max = new double[] { -Double.MAX_VALUE, -Double.MAX_VALUE, -Double.MAX_VALUE };
+
+		final double[] tmpMin = new double[ 2 ];
+		final double[] tmpMax = new double[ 2 ];
+
+		final AffineModel2D scaleModel = new AffineModel2D();
+		scaleModel.set( scale[ 0 ], 0, 0, scale[ 1 ], 0, 0 );
+
+		// get bounding box
+		for ( final String tileId : idToModels.keySet() )
+		{
+			final MinimalTileSpec tileSpec = idToTileSpec.get( tileId );
+			min[ 2 ] = Math.min( min[ 2 ], tileSpec.getZ() * scale[ 2 ] );
+			max[ 2 ] = Math.max( max[ 2 ], tileSpec.getZ() * scale[ 2 ] );
+
+			final int w = tileSpec.getWidth();
+			final int h = tileSpec.getHeight();
+
+			final AffineModel2D model = idToModels.get( tileId ).copy();
+
+			// scale the actual transform down to the scale level we want to render in
+			model.preConcatenate( scaleModel );
+
+			tmpMin[ 0 ] = 0;
+			tmpMin[ 1 ] = 0;
+			tmpMax[ 0 ] = w - 1;
+			tmpMax[ 1 ] = h - 1;
+
+			model.estimateBounds( tmpMin, tmpMax );
+
+			min[ 0 ] = Math.min( min[ 0 ], Math.min( tmpMin[ 0 ], tmpMax[ 0 ] ) );
+			max[ 0 ] = Math.max( max[ 0 ], Math.max( tmpMin[ 0 ], tmpMax[ 0 ] ) );
+
+			min[ 1 ] = Math.min( min[ 1 ], Math.min( tmpMin[ 1 ], tmpMax[ 1 ] ) );
+			max[ 1 ] = Math.max( max[ 1 ], Math.max( tmpMin[ 1 ], tmpMax[ 1 ] ) );
+
+			if ( idToInvertedRenderModels != null )
+				idToInvertedRenderModels.put( tileId, model.createInverse() );
+		}
+
+		//System.out.println( "x: " + min[ 0 ] + " >>> " + max[ 0 ] );
+		//System.out.println( "y: " + min[ 1 ] + " >>> " + max[ 1 ] );
+		//System.out.println( "z: " + min[ 2 ] + " >>> " + max[ 2 ] );
+
+		final long[] minI = new long[ 3 ];
+		final long[] maxI = new long[ 3 ];
+		final long[] dimI = new long[ 3 ];
+
+		for ( int d = 0; d < minI.length; ++d )
+		{
+			minI[ d ] = Math.round( Math.floor( min[ d ] ) );
+			maxI[ d ] = Math.round( Math.ceil( max[ d ] ) );
+			dimI[ d ] = maxI[ d ] - minI[ d ] + 1;
+		}
+
+		//System.out.println( "BB x: " + minI[ 0 ] + " >>> " + maxI[ 0 ] + ", d=" + dimI[ 0 ] );
+		//System.out.println( "BB y: " + minI[ 1 ] + " >>> " + maxI[ 1 ] + ", d=" + dimI[ 1 ]);
+		//System.out.println( "BB z: " + minI[ 2 ] + " >>> " + maxI[ 2 ] + ", d=" + dimI[ 2 ]);
+
+		return new FinalInterval( minI, maxI );
+	}
 }
