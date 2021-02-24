6b4d034e686ba9c38db67a0d118d8a301fe1ed1e
Stephan Preibisch <preibischs@janelia.hhmi.org>
2021-02-24 08:30:02 -0500

    aligning Sec38_BR

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/DistributedSolveMultiThread.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/DistributedSolveMultiThread.java
@@ -114,8 +114,8 @@ public class DistributedSolveMultiThread extends DistributedSolve
                     final String[] testArgs = {
                             "--baseDataUrl", "http://tem-services.int.janelia.org:8080/render-ws/v1",
                             "--owner", "Z0720_07m_BR", //"flyem", //"cosem", //"Z1217_33m_BR",
-                            "--project", "Sec39", //"Z0419_25_Alpha3", //"jrc_hela_2", //"Sec10",
-                            "--matchCollection", "Sec39_v1", //"Sec32_v1", //"Z0419_25_Alpha3_v1", //"jrc_hela_2_v1", //"Sec10_multi",
+                            "--project", "Sec38", //"Z0419_25_Alpha3", //"jrc_hela_2", //"Sec10",
+                            "--matchCollection", "Sec38_v1", //"Sec32_v1", //"Z0419_25_Alpha3_v1", //"jrc_hela_2_v1", //"Sec10_multi",
                             "--stack", "v1_acquire_trimmed", //"v3_acquire",
                             "--targetStack", "v1_acquire_trimmed_sp1",
                             "--completeTargetStack",
@@ -132,7 +132,7 @@ public class DistributedSolveMultiThread extends DistributedSolve
                             //"--noStitching", // do not stitch first
                             
                             "--minZ", "1",
-                            "--maxZ", "27499", //"9505", //"6480",//"34022",
+                            "--maxZ", "34985", //"9505", //"6480",//"34022",
 
                             "--maxNumMatches", "0", // no limit, default
                             "--threadsWorker", "1", 
