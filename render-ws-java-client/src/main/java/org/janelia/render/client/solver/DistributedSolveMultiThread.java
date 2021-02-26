5f732612d48942438a00595ebaf6e624a114aee8
Stephan Preibisch <preibischs@janelia.hhmi.org>
2021-02-26 13:48:35 -0500

    aligning Sec37

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/DistributedSolveMultiThread.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/DistributedSolveMultiThread.java
@@ -114,8 +114,8 @@ public class DistributedSolveMultiThread extends DistributedSolve
                     final String[] testArgs = {
                             "--baseDataUrl", "http://tem-services.int.janelia.org:8080/render-ws/v1",
                             "--owner", "Z0720_07m_BR", //"flyem", //"cosem", //"Z1217_33m_BR",
-                            "--project", "Sec38", //"Z0419_25_Alpha3", //"jrc_hela_2", //"Sec10",
-                            "--matchCollection", "Sec38_v1", //"Sec32_v1", //"Z0419_25_Alpha3_v1", //"jrc_hela_2_v1", //"Sec10_multi",
+                            "--project", "Sec37", //"Z0419_25_Alpha3", //"jrc_hela_2", //"Sec10",
+                            "--matchCollection", "Sec37_v1", //"Sec32_v1", //"Z0419_25_Alpha3_v1", //"jrc_hela_2_v1", //"Sec10_multi",
                             "--stack", "v1_acquire_trimmed", //"v3_acquire",
                             "--targetStack", "v1_acquire_trimmed_sp1",
                             "--completeTargetStack",
@@ -132,11 +132,11 @@ public class DistributedSolveMultiThread extends DistributedSolve
                             //"--noStitching", // do not stitch first
                             
                             "--minZ", "1",
-                            "--maxZ", "34985", //"9505", //"6480",//"34022",
+                            "--maxZ", "37213", //"9505", //"6480",//"34022",
 
                             "--maxNumMatches", "0", // no limit, default
                             "--threadsWorker", "1", 
-                            "--threadsGlobal", "72",
+                            "--threadsGlobal", "60",
                             "--maxPlateauWidthGlobal", "50",
                             "--maxIterationsGlobal", "10000",
 							//"--serializerDirectory", ".",
@@ -200,6 +200,9 @@ public class DistributedSolveMultiThread extends DistributedSolve
 						gs.idToFinalModelGlobal,
 						gs.idToTileSpecGlobal,
 						vis, 36 );
+				vis.setDisplayRange(0, 255 );
+				vis.setCurrent();
+				
 
                 SimpleMultiThreading.threadHaltUnClean();
             }
