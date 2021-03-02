f56973cd71e43086333816aa2fe6eeb094d15fe5
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-03-02 09:54:06 -0500

    moved testing code for additional transforms on top of render loading into UpdatingRenderRA.java

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/interactive/Unbend.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/interactive/Unbend.java
@@ -11,201 +11,19 @@ import java.util.stream.Collectors;
 import org.janelia.alignment.spec.stack.StackMetaData;
 import org.janelia.alignment.util.ImageProcessorCache;
 import org.janelia.render.client.solver.visualize.RenderTools;
-import org.janelia.render.client.solver.visualize.lazy.UpdatingRenderRA;
 
 import bdv.util.BdvStackSource;
-import ij.ImageJ;
-import ij.ImagePlus;
-import mpicbg.trakem2.transform.TransformMeshMappingWithMasks.ImageProcessorWithMasks;
 import net.imglib2.Interval;
-import net.imglib2.RandomAccessible;
-import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealPoint;
 import net.imglib2.cache.Invalidate;
-import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
 import net.imglib2.realtransform.AffineTransform2D;
-import net.imglib2.realtransform.RealViews;
-import net.imglib2.realtransform.Scale2D;
-import net.imglib2.realtransform.Translation2D;
-import net.imglib2.util.Intervals;
 import net.imglib2.util.Pair;
 import net.imglib2.util.RealSum;
 import net.imglib2.util.Util;
 import net.imglib2.util.ValuePair;
-import net.imglib2.view.Views;
 
 public class Unbend
 {
-	private static void testAdditionalTransform(
-			final String baseUrl,
-			final String owner,
-			final String project,
-			final String stack )
-	{
-		// test applying transform
-		final boolean filter = false;
-
-		// always in world coordinates
-		int w = 512;
-		int h = 512;
-		int x = 10000;
-		int y = 1000;
-		int z = 6151;
-
-		double scale = 1.0 /8.0;
-		ImageProcessorWithMasks img1 = RenderTools.renderImage( null, baseUrl, owner, project, stack, x, y, z, w, h, scale, filter );
-
-		new ImageJ();
-		final ImagePlus imp1 = new ImagePlus("img1", img1.ip);
-		imp1.show();
-
-		// world coordinates
-		AffineTransform2D t = new AffineTransform2D();
-		// simple translation test
-		//t.translate( 256, 256 );
-
-		// rotate by 45 degrees around the center of this patch
-		t.translate( -(x + w/2.0), -(y + h/2.0) );
-		t.rotate( Math.toRadians( 45 ) );
-		t.translate( (x + w/2.0), (y + h/2.0) );
-
-		// which part of the image do we need to fetch?
-		final Interval blockInterval = Intervals.createMinMax( x, y, x + w - 1, y + h - 1 );
-		final Interval transformedInterval =
-				Intervals.smallestContainingInterval(
-						UpdatingRenderRA.estimateBounds( t.inverse(), blockInterval ) );
-
-		System.out.println( Util.printInterval( blockInterval ) );
-		System.out.println( Util.printInterval( transformedInterval ) );
-
-		final ImageProcessorWithMasks ipm = RenderTools.renderImage(
-				null, baseUrl, owner, project, stack,
-				transformedInterval.min( 0 ),
-				transformedInterval.min( 1 ),
-				z,
-				transformedInterval.dimension( 0 ),
-				transformedInterval.dimension( 1 ),
-				scale,
-				false );
-
-		RandomAccessibleInterval fromRender;
-
-		if ( ipm.ip.getBitDepth() == 8 )
-		{
-			fromRender = ArrayImgs.unsignedBytes( (byte[])ipm.ip.getPixels(), ipm.ip.getWidth(), ipm.ip.getHeight() );
-		}
-		else if ( ipm.ip.getBitDepth() == 16 )
-		{
-			fromRender = ArrayImgs.unsignedShorts( (short[])ipm.ip.getPixels(), ipm.ip.getWidth(), ipm.ip.getHeight() );
-		}
-		else if ( ipm.ip.getBitDepth() == 32 )
-		{
-			fromRender = ArrayImgs.floats( (float[])ipm.ip.getPixels(), ipm.ip.getWidth(), ipm.ip.getHeight() );
-		}
-		else
-		{
-			throw new RuntimeException( "imgtype " + ipm.ip.getBitDepth() + " not supported." );
-		}
-
-		ImageJFunctions.show( fromRender );
-
-		// make the downsampled image sitting at 0,0 a full-scale version of itself
-		AffineTransform2D correctImage = new AffineTransform2D();
-		Translation2D offsetT = new Translation2D( transformedInterval.min( 0 ), transformedInterval.min( 1 ) );
-		Scale2D scaleT = new Scale2D( scale, scale );
-
-		correctImage = correctImage.preConcatenate( scaleT.inverse() );
-		correctImage = correctImage.preConcatenate( offsetT );
-
-		// before applying our transformation we make the downsampled block a full-scale image
-		t = t.concatenate( correctImage );
-
-		// now after applying the transform, we scale down again
-		t = t.preConcatenate( scaleT );
-
-		// TODO: do we need to scale t here since the result is a small block
-		RandomAccessible transformed = RealViews.affine( Views.interpolate( Views.extendZero( fromRender ), new NLinearInterpolatorFactory() ), t );
-
-		RandomAccessibleInterval finalImg = Views.interval( transformed, Intervals.createMinSize( Math.round( x*scale ), Math.round(y*scale), Math.round(w*scale), Math.round(h*scale)) );
-
-		ImageJFunctions.show( finalImg );
-		
-	}
-
-	public static void main( String[] args ) throws IOException
-	{
-		String baseUrl = "http://tem-services.int.janelia.org:8080/render-ws/v1";
-		String owner = "Z0720_07m_VNC"; //"flyem";
-		String project = "Sec32"; //"Z0419_25_Alpha3";
-		String stack = "v1_acquire_trimmed_sp1"; //"v1_acquire_sp_nodyn_v2";
-		String matchCollection = "Sec32_v1";
-
-		//testAdditionalTransform(baseUrl, owner, project, stack);
-		//SimpleMultiThreading.threadHaltUnClean();
-
-		StackMetaData meta = RenderTools.openStackMetaData(baseUrl, owner, project, stack);
-		Interval interval = RenderTools.stackBounds( meta );
-
-		final boolean recordStats = true;
-
-		// only cache original imageProcessors if you don't have mipmaps
-		final boolean cacheOriginalsForDownSampledImages = false;
-		// make imageProcessor cache large enough for masks and some images, but leave most RAM for BDV
-		final long cachedPixels = 2000000;
-		final ImageProcessorCache ipCache = new ImageProcessorCache( cachedPixels, recordStats, cacheOriginalsForDownSampledImages );
-
-		// make most cores available for viewer
-		final double totalThreadUsage = 0.8;
-		final int numTotalThreads = (int) Math.floor(Runtime.getRuntime().availableProcessors() * totalThreadUsage);
-
-		// TODO: determine optimal distribution of threads between render and fetch (using half and half for now)
-		final int numFetchThreads = Math.max(numTotalThreads / 2, 1);
-		final int numRenderingThreads = Math.max(numTotalThreads - numFetchThreads, 1);
-
-		// at first just identity transform, later update to use the 
-		final Unbending unbending = new Unbending();
-		final ArrayList< Invalidate<?> > caches = new ArrayList<>();
-
-		List< double[] > points = new ArrayList<>();
-
-		points.add( new double[] {18180.08737355983, 1044.0922756053733, -62.756698404699364});
-		points.add( new double[] {18180.08737355983, 1475.0200175143727, 749.2973265866995});
-		points.add( new double[] {18189.853698805466, 1724.0546629692826, 1682.3963054607357});
-		points.add( new double[] {18216.26065146877, 1791.5061812018662, 4593.851412228383});
-		points.add( new double[] {18026.55565070638, 2470.9690334880047, 6412.092765652576});
-		points.add( new double[] {18026.55565070639, 3128.7214768764475, 7769.737739455516});
-		points.add( new double[] {18026.55565070639, 4020.1983406595014, 9125.525469792246});
-		points.add( new double[] {18044.623199414567, 4496.445482984773, 10431.538583396674});
-		points.add( new double[] {18069.563192165682, 4781.255819355375, 12703.129389095324});
-
-		BdvStackSource<?> bdv = RenderTools.renderMultiRes(
-				ipCache, baseUrl, owner, project, stack, interval, null, numRenderingThreads, numFetchThreads,
-				unbending, caches );
-		bdv.setDisplayRange( 0, 256 );
-
-		InteractiveSegmentedLine line = new InteractiveSegmentedLine( bdv, points );
-		points = line.getResult();
-
-		if ( points != null && points.size() > 0 )
-		{
-			for ( final double[] p : points )
-				System.out.println( Util.printCoordinates( p ) );
-
-			new VisualizeSegmentedLine( bdv, points, Color.yellow, Color.yellow.darker(), null ).install();
-		}
-
-		final ArrayList<Pair<Integer, double[]>> positions = positionPerZSlice(points, interval.min( 2 ), interval.max( 2 ), 0.01 );
-		final double[] avg = new double[ 2 ];
-
-		unbending.setTranslations( centerTranslations( positions, avg ));
-
-		updatePoints( points, avg );
-		caches.forEach( c -> c.invalidateAll() );
-		bdv.getBdvHandle().getViewerPanel().requestRepaint();
-	}
-
 	protected static void updatePoints( List< double[] > points, final double[] avg )
 	{
 		// spline goes through the points
@@ -321,7 +139,7 @@ public class Unbend
 
 		return (z1-z0)/1E-8;
 	}
-	
+
 	public static class Unbending implements Function<Integer, AffineTransform2D>
 	{
 		AffineTransform2D identity = new AffineTransform2D();
@@ -353,6 +171,75 @@ public class Unbend
 			t.translate( 10000, 1000 );
 			return t;*/
 		}
-		
 	}
+
+	public static void main( String[] args ) throws IOException
+	{
+		String baseUrl = "http://tem-services.int.janelia.org:8080/render-ws/v1";
+		String owner = "Z0720_07m_VNC"; //"flyem";
+		String project = "Sec32"; //"Z0419_25_Alpha3";
+		String stack = "v1_acquire_trimmed_sp1"; //"v1_acquire_sp_nodyn_v2";
+		//String matchCollection = "Sec32_v1";
+
+		StackMetaData meta = RenderTools.openStackMetaData(baseUrl, owner, project, stack);
+		Interval interval = RenderTools.stackBounds( meta );
+
+		final boolean recordStats = true;
+
+		// only cache original imageProcessors if you don't have mipmaps
+		final boolean cacheOriginalsForDownSampledImages = false;
+		// make imageProcessor cache large enough for masks and some images, but leave most RAM for BDV
+		final long cachedPixels = 2000000;
+		final ImageProcessorCache ipCache = new ImageProcessorCache( cachedPixels, recordStats, cacheOriginalsForDownSampledImages );
+
+		// make most cores available for viewer
+		final double totalThreadUsage = 0.8;
+		final int numTotalThreads = (int) Math.floor(Runtime.getRuntime().availableProcessors() * totalThreadUsage);
+
+		// TODO: determine optimal distribution of threads between render and fetch (using half and half for now)
+		final int numFetchThreads = Math.max(numTotalThreads / 2, 1);
+		final int numRenderingThreads = Math.max(numTotalThreads - numFetchThreads, 1);
+
+		// at first just identity transform, later update to use the 
+		final Unbending unbending = new Unbending();
+		final ArrayList< Invalidate<?> > caches = new ArrayList<>();
+
+		List< double[] > points = new ArrayList<>();
+
+		points.add( new double[] {18180.08737355983, 1044.0922756053733, -62.756698404699364});
+		points.add( new double[] {18180.08737355983, 1475.0200175143727, 749.2973265866995});
+		points.add( new double[] {18189.853698805466, 1724.0546629692826, 1682.3963054607357});
+		points.add( new double[] {18216.26065146877, 1791.5061812018662, 4593.851412228383});
+		points.add( new double[] {18026.55565070638, 2470.9690334880047, 6412.092765652576});
+		points.add( new double[] {18026.55565070639, 3128.7214768764475, 7769.737739455516});
+		points.add( new double[] {18026.55565070639, 4020.1983406595014, 9125.525469792246});
+		points.add( new double[] {18044.623199414567, 4496.445482984773, 10431.538583396674});
+		points.add( new double[] {18069.563192165682, 4781.255819355375, 12703.129389095324});
+
+		BdvStackSource<?> bdv = RenderTools.renderMultiRes(
+				ipCache, baseUrl, owner, project, stack, interval, null, numRenderingThreads, numFetchThreads,
+				unbending, caches );
+		bdv.setDisplayRange( 0, 256 );
+
+		InteractiveSegmentedLine line = new InteractiveSegmentedLine( bdv, points );
+		points = line.getResult();
+
+		if ( points != null && points.size() > 0 )
+		{
+			for ( final double[] p : points )
+				System.out.println( Util.printCoordinates( p ) );
+
+			new VisualizeSegmentedLine( bdv, points, Color.yellow, Color.yellow.darker(), null ).install();
+		}
+
+		final ArrayList<Pair<Integer, double[]>> positions = positionPerZSlice(points, interval.min( 2 ), interval.max( 2 ), 0.01 );
+		final double[] avg = new double[ 2 ];
+
+		unbending.setTranslations( centerTranslations( positions, avg ));
+
+		updatePoints( points, avg );
+		caches.forEach( c -> c.invalidateAll() );
+		bdv.getBdvHandle().getViewerPanel().requestRepaint();
+	}
+
 }
--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/visualize/lazy/UpdatingRenderRA.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/visualize/lazy/UpdatingRenderRA.java
@@ -1,10 +1,13 @@
 package org.janelia.render.client.solver.visualize.lazy;
 
+import java.io.IOException;
 import java.util.function.Function;
 
 import org.janelia.alignment.util.ImageProcessorCache;
 import org.janelia.render.client.solver.visualize.RenderTools;
 
+import ij.ImageJ;
+import ij.ImagePlus;
 import mpicbg.trakem2.transform.TransformMeshMappingWithMasks.ImageProcessorWithMasks;
 import net.imglib2.Cursor;
 import net.imglib2.FinalRealInterval;
@@ -13,6 +16,7 @@ import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.RealInterval;
 import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.realtransform.RealViews;
@@ -200,4 +204,109 @@ public class UpdatingRenderRA<T extends RealType<T> & NativeType<T>> extends Ren
 		return new FinalRealInterval( rMin, rMax );
 	}
 
+	private static void testAdditionalTransform(
+			final String baseUrl,
+			final String owner,
+			final String project,
+			final String stack )
+	{
+		// test applying transform
+		final boolean filter = false;
+
+		// always in world coordinates
+		int w = 512;
+		int h = 512;
+		int x = 10000;
+		int y = 1000;
+		int z = 6151;
+
+		double scale = 1.0 /8.0;
+		ImageProcessorWithMasks img1 = RenderTools.renderImage( null, baseUrl, owner, project, stack, x, y, z, w, h, scale, filter );
+
+		new ImageJ();
+		final ImagePlus imp1 = new ImagePlus("img1", img1.ip);
+		imp1.show();
+
+		// world coordinates
+		AffineTransform2D t = new AffineTransform2D();
+		// simple translation test
+		//t.translate( 256, 256 );
+
+		// rotate by 45 degrees around the center of this patch
+		t.translate( -(x + w/2.0), -(y + h/2.0) );
+		t.rotate( Math.toRadians( 45 ) );
+		t.translate( (x + w/2.0), (y + h/2.0) );
+
+		// which part of the image do we need to fetch?
+		final Interval blockInterval = Intervals.createMinMax( x, y, x + w - 1, y + h - 1 );
+		final Interval transformedInterval =
+				Intervals.smallestContainingInterval(
+						UpdatingRenderRA.estimateBounds( t.inverse(), blockInterval ) );
+
+		System.out.println( Util.printInterval( blockInterval ) );
+		System.out.println( Util.printInterval( transformedInterval ) );
+
+		final ImageProcessorWithMasks ipm = RenderTools.renderImage(
+				null, baseUrl, owner, project, stack,
+				transformedInterval.min( 0 ),
+				transformedInterval.min( 1 ),
+				z,
+				transformedInterval.dimension( 0 ),
+				transformedInterval.dimension( 1 ),
+				scale,
+				false );
+
+		RandomAccessibleInterval fromRender;
+
+		if ( ipm.ip.getBitDepth() == 8 )
+		{
+			fromRender = ArrayImgs.unsignedBytes( (byte[])ipm.ip.getPixels(), ipm.ip.getWidth(), ipm.ip.getHeight() );
+		}
+		else if ( ipm.ip.getBitDepth() == 16 )
+		{
+			fromRender = ArrayImgs.unsignedShorts( (short[])ipm.ip.getPixels(), ipm.ip.getWidth(), ipm.ip.getHeight() );
+		}
+		else if ( ipm.ip.getBitDepth() == 32 )
+		{
+			fromRender = ArrayImgs.floats( (float[])ipm.ip.getPixels(), ipm.ip.getWidth(), ipm.ip.getHeight() );
+		}
+		else
+		{
+			throw new RuntimeException( "imgtype " + ipm.ip.getBitDepth() + " not supported." );
+		}
+
+		ImageJFunctions.show( fromRender );
+
+		// make the downsampled image sitting at 0,0 a full-scale version of itself
+		AffineTransform2D correctImage = new AffineTransform2D();
+		Translation2D offsetT = new Translation2D( transformedInterval.min( 0 ), transformedInterval.min( 1 ) );
+		Scale2D scaleT = new Scale2D( scale, scale );
+
+		correctImage = correctImage.preConcatenate( scaleT.inverse() );
+		correctImage = correctImage.preConcatenate( offsetT );
+
+		// before applying our transformation we make the downsampled block a full-scale image
+		t = t.concatenate( correctImage );
+
+		// now after applying the transform, we scale down again
+		t = t.preConcatenate( scaleT );
+
+		// TODO: do we need to scale t here since the result is a small block
+		RandomAccessible transformed = RealViews.affine( Views.interpolate( Views.extendZero( fromRender ), new NLinearInterpolatorFactory() ), t );
+
+		RandomAccessibleInterval finalImg = Views.interval( transformed, Intervals.createMinSize( Math.round( x*scale ), Math.round(y*scale), Math.round(w*scale), Math.round(h*scale)) );
+
+		ImageJFunctions.show( finalImg );
+		
+	}
+
+	public static void main( String[] args ) throws IOException
+	{
+		String baseUrl = "http://tem-services.int.janelia.org:8080/render-ws/v1";
+		String owner = "Z0720_07m_VNC"; //"flyem";
+		String project = "Sec32"; //"Z0419_25_Alpha3";
+		String stack = "v1_acquire_trimmed_sp1"; //"v1_acquire_sp_nodyn_v2";
+
+		testAdditionalTransform(baseUrl, owner, project, stack);
+	}
 }
