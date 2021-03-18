1e12cd874c512d4a0d790cffa10f9b31928e6fb9
Eric Trautman <trautmane@janelia.hhmi.org>
2021-03-18 15:12:18 -0400

    update test parameters to match SP request

--- render-ws-java-client/src/test/java/org/janelia/render/client/zspacing/ZPositionCorrectionClientTest.java
+++ render-ws-java-client/src/test/java/org/janelia/render/client/zspacing/ZPositionCorrectionClientTest.java
@@ -17,20 +17,22 @@ public class ZPositionCorrectionClientTest {
 
     public static void main(final String[] args) {
 
+        final String userHome = System.getProperty("user.home");
         final String[] effectiveArgs = (args != null) && (args.length > 0) ? args : new String[]{
                 "--baseDataUrl", "http://renderer-dev.int.janelia.org:8080/render-ws/v1",
-                "--owner", "Z0720_07m_VNC",
-                "--project", "Sec32",
+                "--owner", "Z0720_07m_BR",
+                "--project", "Sec39",
                 "--stack", "v1_acquire_trimmed_sp1",
-                "--scale", "0.3",
-                "--minZ", "4600.0",
-                "--maxZ", "4699.0",
-                "--minX", "10740",
-                "--maxX", "14740",
-                "--minY", "1900",
-                "--maxY", "3200",
+                "--scale", "0.25",
+                "--minZ", "19950",
+                "--maxZ", "20050",
+//                "--minX", "2500",
+//                "--maxX", "5500",
+//                "--minY", "400",
+//                "--maxY", "1700",
 //                "--debugFormat", "png",
-                "--rootDirectory", System.getProperty("user.home") + "/Desktop/zcorr"
+//                "--optionsJson", userHome + "/Desktop/inference-options.json",
+                "--rootDirectory", userHome + "/Desktop/zcorr"
         };
 
         ZPositionCorrectionClient.main(effectiveArgs);
