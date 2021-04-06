95d773f7373bf357ce342538a37dd1e54fa9b75d
Eric Trautman <trautmane@janelia.hhmi.org>
2021-04-06 11:58:52 -0400

    fix ShortRendererTest to use 16-bit source images

--- render-app/src/test/java/org/janelia/alignment/ShortRendererTest.java
+++ render-app/src/test/java/org/janelia/alignment/ShortRendererTest.java
@@ -1,19 +1,3 @@
-/**
- * License: GPL
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
 package org.janelia.alignment;
 
 import java.io.File;
@@ -52,14 +36,14 @@ public class ShortRendererTest {
     public void testStitching() throws Exception {
 
         final File expectedFile =
-                new File(modulePath + "/src/test/resources/stitch-test/expected_stitched_4_tiles_16_bit.png");
+                new File(modulePath + "/src/test/resources/stitch-test/expected_stitched_16_bit.png");
 
         final String[] args = {
-                "--tile_spec_url", "src/test/resources/stitch-test/test_4_tiles.json",
+                "--tile_spec_url", "src/test/resources/stitch-test/16_bit_tiles.json",
                 "--out", outputFile.getAbsolutePath(),
-                "--width", "4576",
-                "--height", "4173",
-                "--scale", "0.05"
+                "--width", "700",
+                "--height", "400",
+                "--scale", "0.5"
         };
 
         ShortRenderer.renderUsingCommandLineArguments(args);
