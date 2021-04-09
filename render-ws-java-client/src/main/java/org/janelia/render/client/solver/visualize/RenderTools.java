2ed7c63d0d5ea232650919456e5ae0e4c930519a
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-04-09 16:33:37 -0400

    looking at cosem sample

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/visualize/RenderTools.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/visualize/RenderTools.java
@@ -354,9 +354,9 @@ public class RenderTools
 	public static void main( String[] args ) throws IOException
 	{
 		String baseUrl = "http://tem-services.int.janelia.org:8080/render-ws/v1";
-		String owner = "Z0720_07m_BR"; //"flyem";
-		String project = "Sec39"; //"Z0419_25_Alpha3";
-		String stack = "v1_acquire_trimmed_sp1"; //"v1_acquire_sp_nodyn_v2";
+		String owner = "cosem"; //"flyem";
+		String project = "aic_desmosome_2"; //"Z0419_25_Alpha3";
+		String stack = "v1_acquire_align_adaptive_2_straight2";
 
 		StackMetaData meta = openStackMetaData(baseUrl, owner, project, stack);
 		
