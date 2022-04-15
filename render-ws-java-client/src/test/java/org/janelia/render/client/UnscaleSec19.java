62689aeedf37c4c2a86a3c120bf3452f10b4f610
Eric Trautman <trautmane@janelia.hhmi.org>
2022-04-15 12:36:26 -0400

    add clipped render code from CrossCorrelationPointMatchClient to UnscaleSec19 test for Preibisch

--- render-ws-java-client/src/test/java/org/janelia/render/client/UnscaleSec19.java
+++ render-ws-java-client/src/test/java/org/janelia/render/client/UnscaleSec19.java
@@ -1,23 +1,32 @@
 package org.janelia.render.client;
 
-import java.awt.Rectangle;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.janelia.alignment.match.CanvasCorrelationMatcher;
-import org.janelia.alignment.match.parameters.CrossCorrelationParameters;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import ij.ImageJ;
 import ij.ImagePlus;
 import ij.gui.Roi;
 import ij.process.ImageProcessor;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
 import mpicbg.models.Point;
 import mpicbg.models.PointMatch;
 import mpicbg.stitching.PairWiseStitchingImgLib;
 import mpicbg.stitching.PairWiseStitchingResult;
 import mpicbg.stitching.StitchingParameters;
+import mpicbg.trakem2.transform.TransformMeshMappingWithMasks;
+
+import org.janelia.alignment.match.CanvasId;
+import org.janelia.alignment.match.CanvasIdWithRenderContext;
+import org.janelia.alignment.match.CanvasRenderParametersUrlTemplate;
+import org.janelia.alignment.match.MontageRelativePosition;
+import org.janelia.alignment.match.parameters.CrossCorrelationParameters;
+import org.janelia.alignment.match.parameters.FeatureRenderClipParameters;
+import org.janelia.alignment.match.parameters.FeatureRenderParameters;
+import org.janelia.alignment.util.ImageProcessorCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.imglib2.util.Util;
 
 public class UnscaleSec19 {
@@ -34,20 +43,80 @@ public class UnscaleSec19 {
 		ccParameters.subpixelAccuracy = true;
 		final double renderScale = 0.25;
 
-		// how to load the images at renderscale and crop to the appromximately overlapping area
+		// Adapted clipped render code from CrossCorrelationPointMatchClient below.
+		// Minimally, must mount dm11 flyem at /groups/...
+		// To make things faster (but not required), also mount nrs flyem to get access to mipmaps.
+
+		final long maximumNumberOfCachedSourcePixels = 1_000_000_000; // 1GB
+		final ImageProcessorCache sourceImageProcessorCache =
+				new ImageProcessorCache(maximumNumberOfCachedSourcePixels,
+										true,
+										false);
+
+		final String baseDataUrlString = "http://renderer-dev.int.janelia.org:8080/render-ws/v1";
+		final String owner = "Z0720_07m_VNC";
+		final String project = "Sec19";
+		final String stack = "v1_acquire";
+		final String generalTemplateString = baseDataUrlString + "/owner/" + owner + "/project/" + project +
+											 "/stack/" + stack + "/tile/{id}/render-parameters";
+
+		final FeatureRenderParameters featureRenderParameters = new FeatureRenderParameters();
+		featureRenderParameters.renderScale = renderScale;
+		featureRenderParameters.renderWithoutMask = false;
+
+		final FeatureRenderClipParameters featureRenderClipParameters = new FeatureRenderClipParameters();
+		featureRenderClipParameters.clipHeight = 500;
+		featureRenderClipParameters.clipWidth = 500;
+
+		final CanvasRenderParametersUrlTemplate urlTemplateForRun =
+				CanvasRenderParametersUrlTemplate.getTemplateForRun(
+						generalTemplateString,
+						featureRenderParameters,
+						featureRenderClipParameters);
 
 		//layer 3781 of Sec19, tile 0 and 1
-		ImagePlus imp1 = new ImagePlus("/Users/preibischs/Documents/Janelia/Projects/Male CNS+VNC Alignment/07m/VNC-Sec 19/Merlin-6262_21-09-20_094008_0-0-0-InLens.png");
-		ImagePlus imp2 = new ImagePlus("/Users/preibischs/Documents/Janelia/Projects/Male CNS+VNC Alignment/07m/VNC-Sec 19/Merlin-6262_21-09-20_094008_0-0-1-InLens.png"); 
-		ImagePlus mask1 = new ImagePlus("/Users/preibischs/Documents/Janelia/Projects/Male CNS+VNC Alignment/07m/VNC-Sec 19/mask_7687x3500_left_100.tif");
-		ImagePlus mask2 = new ImagePlus("/Users/preibischs/Documents/Janelia/Projects/Male CNS+VNC Alignment/07m/VNC-Sec 19/mask_7687x3500_left_100.tif");
+//		ImagePlus imp1 = new ImagePlus("/Users/preibischs/Documents/Janelia/Projects/Male CNS+VNC Alignment/07m/VNC-Sec 19/Merlin-6262_21-09-20_094008_0-0-0-InLens.png");
+//		ImagePlus imp2 = new ImagePlus("/Users/preibischs/Documents/Janelia/Projects/Male CNS+VNC Alignment/07m/VNC-Sec 19/Merlin-6262_21-09-20_094008_0-0-1-InLens.png");
+//		ImagePlus mask1 = new ImagePlus("/Users/preibischs/Documents/Janelia/Projects/Male CNS+VNC Alignment/07m/VNC-Sec 19/mask_7687x3500_left_100.tif");
+//		ImagePlus mask2 = new ImagePlus("/Users/preibischs/Documents/Janelia/Projects/Male CNS+VNC Alignment/07m/VNC-Sec 19/mask_7687x3500_left_100.tif");
+
+		final CanvasId pCanvasId = new CanvasId("3781.0",
+												"21-09-20_094008_0-0-0.3781.0",
+												MontageRelativePosition.LEFT);
+		final CanvasId qCanvasId = new CanvasId("3781.0",
+												"21-09-20_094008_0-0-1.3781.0",
+												MontageRelativePosition.RIGHT);
+
+		final CanvasIdWithRenderContext p = CanvasIdWithRenderContext.build(pCanvasId, urlTemplateForRun);
+		final CanvasIdWithRenderContext q = CanvasIdWithRenderContext.build(qCanvasId, urlTemplateForRun);
+
+		final TransformMeshMappingWithMasks.ImageProcessorWithMasks
+				renderedPCanvas = CrossCorrelationPointMatchClient.renderCanvas(p, sourceImageProcessorCache);
+		final TransformMeshMappingWithMasks.ImageProcessorWithMasks
+				renderedQCanvas = CrossCorrelationPointMatchClient.renderCanvas(q, sourceImageProcessorCache);
+
+		final ImagePlus imp1 = new ImagePlus(pCanvasId.getId(), renderedPCanvas.ip);
+		final ImagePlus imp2 = new ImagePlus(qCanvasId.getId(), renderedQCanvas.ip);
 
 		imp1.show();
 		imp2.show();
-		//mask1.show();
-		//mask2.show();
 
-		getCandidateMatches(imp1,mask1.getProcessor(),imp2,mask2.getProcessor(), true, renderScale, ccParameters );
+		if (renderedPCanvas.mask != null) {
+			final ImagePlus mask1 = new ImagePlus("p mask", renderedPCanvas.mask);
+			mask1.show();
+		}
+		if (renderedQCanvas.mask != null) {
+			final ImagePlus mask2 = new ImagePlus("q mask", renderedPCanvas.mask);
+			mask2.show();
+		}
+
+		getCandidateMatches(imp1,
+							renderedPCanvas.mask,
+							imp2,
+							renderedQCanvas.mask,
+							true,
+							renderScale,
+							ccParameters );
 	}
 
 	private static List<PointMatch> getCandidateMatches(
