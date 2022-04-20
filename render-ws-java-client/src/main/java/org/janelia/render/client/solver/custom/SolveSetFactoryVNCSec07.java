4f8c7fb79c5d198493b8011e62fee74c088b67db
Eric Trautman <trautmane@janelia.hhmi.org>
2022-04-20 09:58:25 -0400

    add custom solvers for VNC Sec07 and VNC Sec12

---
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/custom/SolveSetFactoryVNCSec07.java
@@ -0,0 +1,43 @@
+package org.janelia.render.client.solver.custom;
+
+import java.util.List;
+
+import mpicbg.models.Affine2D;
+
+import org.apache.commons.lang3.Range;
+
+public class SolveSetFactoryVNCSec07
+        extends SolveSetFactoryWithStitchFirstExclusions
+{
+	public SolveSetFactoryVNCSec07(
+			final Affine2D<?> defaultGlobalSolveModel,
+			final Affine2D<?> defaultBlockSolveModel,
+			final Affine2D<?> defaultStitchingModel,
+			final List<Double> defaultBlockOptimizerLambdasRigid,
+			final List<Double> defaultBlockOptimizerLambdasTranslation,
+			final List<Integer> defaultBlockOptimizerIterations,
+			final List<Integer> defaultBlockMaxPlateauWidth,
+			final int defaultMinStitchingInliers,
+			final double defaultBlockMaxAllowedError,
+			final double defaultDynamicLambdaFactor )
+	{
+		super(
+				defaultGlobalSolveModel,
+				defaultBlockSolveModel,
+				defaultStitchingModel,
+				defaultBlockOptimizerLambdasRigid,
+				defaultBlockOptimizerLambdasTranslation,
+				defaultBlockOptimizerIterations,
+				defaultBlockMaxPlateauWidth,
+				defaultMinStitchingInliers,
+				defaultBlockMaxAllowedError,
+				defaultDynamicLambdaFactor );
+
+		final int lastTwoTileZ = 20355;
+		final int exclusionMargin = 50;
+
+		addStitchFirstExclusionRange(Range.between(lastTwoTileZ - exclusionMargin,
+												   lastTwoTileZ + exclusionMargin));
+	}
+
+}
