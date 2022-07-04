f16e1b66dab2f38c7c32f4a566fbf4e228fe7eae
Eric Trautman <trautmane@janelia.hhmi.org>
2022-07-04 16:50:11 -0400

    parameters to straighten vnc sec13

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/interactive/Unbend.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/interactive/Unbend.java
@@ -186,10 +186,9 @@ public class Unbend
 	{
 		String baseUrl = "http://tem-services.int.janelia.org:8080/render-ws/v1";
 		String owner = "Z0720_07m_VNC";
-		String project = "Sec33"; //"Z0419_25_Alpha3";
-		String stack = "v2_acquire_trimmed_align"; //"v1_acquire_sp_nodyn_v2";
-		String targetStack = "v2_acquire_trimmed_align_straightened_sp";
-		//String matchCollection = "Sec32_v1";
+		String project = "Sec13"; //"Z0419_25_Alpha3";
+		String stack = "v3_acquire_trimmed_align"; //"v1_acquire_sp_nodyn_v2";
+		String targetStack = stack + "_straightened";
 
 		final RenderDataClient renderDataClient = new RenderDataClient(baseUrl, owner, project );
 		final StackMetaData meta =  renderDataClient.getStackMetaData( stack );
