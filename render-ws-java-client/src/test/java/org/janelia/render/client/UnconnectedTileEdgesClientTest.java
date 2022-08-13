bda0b4db941b1a0a05ea9fda5acf1d17e1dc9cdb
Eric Trautman <trautmane@janelia.hhmi.org>
2022-08-13 10:27:53 -0400

    refactor UnconnectedColumnClient into UnconnectedTileEdgesClient to handle new multi-row FIBSEM data sets

--- render-ws-java-client/src/test/java/org/janelia/render/client/UnconnectedTileEdgesClientTest.java
+++ render-ws-java-client/src/test/java/org/janelia/render/client/UnconnectedTileEdgesClientTest.java
@@ -4,31 +4,31 @@ import org.janelia.render.client.parameter.CommandLineParameters;
 import org.junit.Test;
 
 /**
- * Tests the {@link UnconnectedColumnClient} class.
+ * Tests the {@link UnconnectedTileEdgesClient} class.
  *
  * @author Eric Trautman
  */
-public class UnconnectedColumnClientTest {
+public class UnconnectedTileEdgesClientTest {
 
     @Test
     public void testParameterParsing() throws Exception {
-        CommandLineParameters.parseHelp(new UnconnectedColumnClient.Parameters());
+        CommandLineParameters.parseHelp(new UnconnectedTileEdgesClient.Parameters());
     }
 
     public static void main(final String[] args) {
 
         final String[] effectiveArgs = (args != null) && (args.length > 0) ? args : new String[]{
                 "--baseDataUrl", "http://renderer-dev:8080/render-ws/v1",
-                "--owner", "Z0720_07m_VNC",
-                "--project", "Sec11",
-                "--stack", "v2_acquire_trimmed",
+                "--owner", "cellmap",
+                "--project", "jrc_mus_liv_zon_1",
+                "--stack", "v1_acquire",
                 "--minZ", "1",
-                "--maxZ", "3000",
-                "--matchCollection", "Sec11_v1",
-                "--maxUnconnectedLayers", "20"
+                "--maxZ", "2000",
+                "--matchCollection", "jrc_mus_liv_zon_1_v1",
+                "--maxUnconnectedLayers", "50"
         };
 
-        UnconnectedColumnClient.main(effectiveArgs);
+        UnconnectedTileEdgesClient.main(effectiveArgs);
     }
     
 }
