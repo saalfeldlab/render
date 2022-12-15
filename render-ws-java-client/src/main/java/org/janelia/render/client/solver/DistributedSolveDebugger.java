33edeea6412359b7092550075e9691d31f7171fe
Eric Trautman <trautmane@janelia.hhmi.org>
2022-12-15 09:10:45 -0500

    setup up distributed solve debugger for wafer 52 data

--- render-ws-java-client/src/main/java/org/janelia/render/client/solver/DistributedSolveDebugger.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/solver/DistributedSolveDebugger.java
@@ -1,22 +1,23 @@
 package org.janelia.render.client.solver;
 
+import ij.ImageJ;
+import ij.ImagePlus;
+
 import java.io.IOException;
 import java.util.List;
 
+import mpicbg.models.Affine2D;
+
 import org.janelia.render.client.ClientRunner;
-import org.janelia.render.client.solver.custom.SolveSetFactoryBRSec32;
 import org.janelia.render.client.solver.visualize.ErrorTools;
-import org.janelia.render.client.solver.visualize.VisualizeTools;
 import org.janelia.render.client.solver.visualize.ErrorTools.ErrorFilter;
 import org.janelia.render.client.solver.visualize.ErrorTools.ErrorType;
 import org.janelia.render.client.solver.visualize.ErrorTools.Errors;
+import org.janelia.render.client.solver.visualize.VisualizeTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import bdv.util.BdvStackSource;
-import ij.ImageJ;
-import ij.ImagePlus;
-import mpicbg.models.Affine2D;
 import net.imglib2.multithreading.SimpleMultiThreading;
 
 public class DistributedSolveDebugger extends DistributedSolve
@@ -33,7 +34,7 @@ public class DistributedSolveDebugger extends DistributedSolve
 	{
 		//this.solveSet.leftItems.get( 44 ).maxZ = 22100;
 		final DistributedSolveWorker< ? extends Affine2D< ? >, ? extends Affine2D< ? >, ? extends Affine2D< ? > > w =
-				this.solveSet.leftItems.get( 0 ).createWorker( //9, 28, 57, 81
+				this.solveSet.leftItems.get( 1 ).createWorker( //9, 28, 57, 81
 						this.solveSet.getMaxId() + 1,
 						runParams.pGroupList,
 						runParams.sectionIdToZMap,
@@ -73,7 +74,7 @@ public class DistributedSolveDebugger extends DistributedSolve
 				VisualizeTools.renderBDV( vis, imp, 0.5 );
 			}
 
-		} catch ( Exception e1 )
+		} catch ( final Exception e1 )
 		{
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -84,7 +85,7 @@ public class DistributedSolveDebugger extends DistributedSolve
 		return null;
 	}
 
-	public static void main( String[] args )
+	public static void main( final String[] args )
 	{
         final ClientRunner clientRunner = new ClientRunner(args) {
             @Override
@@ -96,26 +97,27 @@ public class DistributedSolveDebugger extends DistributedSolve
                 if (args.length == 0) {
                     final String[] testArgs = {
                             "--baseDataUrl", "http://tem-services.int.janelia.org:8080/render-ws/v1",
-                            "--owner", "cellmap", //"hess", //"Z0720_07m_BR", //"flyem", //"cosem", //"Z1217_33m_BR",
-                            "--project", "jrc_mus_liv_zon_1", //"wafer_52c", //"Sec24", //"Z0419_25_Alpha3", //"jrc_hela_2", //"Sec10",
-                            "--matchCollection", "jrc_mus_liv_zon_1_v1", //"wafer_52c_v1", //"Sec24_v1", //"Sec32_v1", //"Z0419_25_Alpha3_v1", //"jrc_hela_2_v1", //"Sec10_multi",
-                            "--stack", "v2_acquire", //"v1_acquire_slab_001_trimmed_mfov_04", //"v4_acquire_trimmed", //"v3_acquire",
-                            //"--targetStack", "v2_acquire_align",
+                            "--owner", "hess",
+                            "--project", "wafer_52_cut_00030_to_00039",
+                            "--matchCollection", "wafer_52_cut_00030_to_00039_v1",
+                            "--stack", "slab_001_all",
+                            //"--targetStack", "comment-out-to-prevent-tile-spec-save",
                             //"--completeTargetStack",
                             
                             // note: prealign is with translation only
                             "--blockOptimizerLambdasRigid",       "1.0,1.0,0.9,0.3,0.01",
                             "--blockOptimizerLambdasTranslation", "1.0,0.0,0.0,0.0,0.0",
-                            "--blockOptimizerIterations", "1000,1000,500,250,250",
-                            "--blockMaxPlateauWidth", "250,250,150,100,100",
+                            "--blockOptimizerIterations", "100,100,50,25,25",  // "1000,1000,500,250,250", // FlyEM
+                            "--blockMaxPlateauWidth", "25,25,15,10,10",        // "250,250,150,100,100",   // FlyEM
 
                             //"--lambdaStitching", "1.0", // allow rigid tile alignment
 
-                            //"--blockSize", "100",
+                            "--blockSize", "3",
+							"--minStitchingInliers", "100000000", // do not stitch first
                             //"--noStitching", // do not stitch first
                             
-                            "--minZ", "2850",
-                            "--maxZ", "2853",
+                            "--minZ", "1246",
+                            "--maxZ", "1250",
 
                             //"--excludeFromRegularization", "1-5,35-534",
                             "--maxNumMatches", "0", // no limit, default
