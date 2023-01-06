4a92bc185770456e22bd1fdcbca2478678c7a024
Eric Trautman <trautmane@janelia.hhmi.org>
2023-01-06 12:05:05 -0500

    replace canvas pairs array with single p and q that is cross z-layer since Preibisch is looking for scan correction parameters that work for same column in adjacent z

--- render-ws-java-client/src/test/java/org/janelia/render/client/tile/RenderTileWithTransformsClientTest.java
+++ render-ws-java-client/src/test/java/org/janelia/render/client/tile/RenderTileWithTransformsClientTest.java
@@ -8,15 +8,12 @@ import java.awt.Rectangle;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import mpicbg.imglib.multithreading.SimpleMultiThreading;
 import mpicbg.trakem2.transform.TransformMeshMappingWithMasks;
 
 import org.janelia.alignment.match.CanvasId;
 import org.janelia.alignment.match.MontageRelativePosition;
-import org.janelia.alignment.match.OrderedCanvasIdPair;
 import org.janelia.alignment.spec.LeafTransformSpec;
 import org.janelia.alignment.spec.TransformSpec;
 import org.janelia.alignment.transform.SEMDistortionTransformA;
@@ -76,14 +73,14 @@ public class RenderTileWithTransformsClientTest {
         parameters.scale = 0.25;
 
         // http://renderer.int.janelia.org:8080/ng/#!%7B%22dimensions%22:%7B%22x%22:%5B8e-9%2C%22m%22%5D%2C%22y%22:%5B8e-9%2C%22m%22%5D%2C%22z%22:%5B8e-9%2C%22m%22%5D%7D%2C%22position%22:%5B-163.37060546875%2C-4313.3564453125%2C1263.5%5D%2C%22crossSectionScale%22:2%2C%22projectionScale%22:32768%2C%22layers%22:%5B%7B%22type%22:%22image%22%2C%22source%22:%7B%22url%22:%22render://http://renderer.int.janelia.org:8080/reiser/Z0422_05_Ocellar/v3_acquire_align%22%2C%22subsources%22:%7B%22default%22:true%2C%22bounds%22:true%7D%2C%22enableDefaultSubsources%22:false%7D%2C%22tab%22:%22source%22%2C%22name%22:%22v3_acquire_align%22%7D%5D%2C%22selectedLayer%22:%7B%22layer%22:%22v3_acquire_align%22%7D%2C%22layout%22:%22xy%22%7D
-        // TODO: Preibisch - add/remove tile pairs
-        final OrderedCanvasIdPair[] tilePairs = new OrderedCanvasIdPair[] {
-        		// TODO: sorry, 0-0-0.1263.0 <> 0-0-0.1263.0 with overlap of ~1000 px
-                buildPair("22-06-17_080526_0-0-0.1263.0",       "22-06-17_080526_0-0-1.1263.0"),
-                buildPair("22-06-17_081143_0-0-0.1264.0",       "22-06-17_081143_0-0-1.1264.0"),
-//                buildPair("22-06-18_034043_0-0-0.2097.0",       "22-06-18_034043_0-0-1.2097.0"),
-//                buildPair("22-06-18_125654_0-0-0.patch.2098.0", "22-06-18_125654_0-0-1.patch.2098.0") // (patched, so really from z 2099)
-        };
+
+        // column 0, left relative position (to column 1) => crop and view right edge of tiles
+        final CanvasId p = new CanvasId("", "22-06-17_080526_0-0-0.1263.0", MontageRelativePosition.LEFT);
+        final CanvasId q = new CanvasId("", "22-06-17_081143_0-0-0.1264.0", MontageRelativePosition.LEFT);
+
+        // tile ids for next problem area (z 2097 to 2098), z 2098 is patched, so really z 2099
+//        final CanvasId p = new CanvasId("", "22-06-18_034043_0-0-0.2097.0", MontageRelativePosition.LEFT);
+//        final CanvasId q = new CanvasId("", "22-06-18_125654_0-0-0.patch.2098.0", MontageRelativePosition.LEFT);
 
         // TODO: Preibisch - add/change scan correction parameters as needed (my test_a is just an example)
         final String[][] testSpecificArgs = {
@@ -98,16 +95,12 @@ public class RenderTileWithTransformsClientTest {
         System.getProperties().setProperty("plugins.dir", "/Applications/Fiji.app/plugins");
         new ImageJ();
 
-        for (final OrderedCanvasIdPair tilePair : tilePairs) {
-            final CanvasId p = tilePair.getP();
-            final CanvasId q = tilePair.getQ();
-            for (final String[] testArgs : testSpecificArgs) {
-                final LeafTransformSpec transformSpec = new LeafTransformSpec(SEMDistortionTransformA.class.getName(),
-                                                                              testArgs[1]);
-                final List<TransformSpec> tileTransforms = Collections.singletonList(transformSpec);
-                showTile(client, tileTransforms, parameters.scale, testArgs[0], p);
-                showTile(client, tileTransforms, parameters.scale, testArgs[0], q);
-            }
+        for (final String[] testArgs : testSpecificArgs) {
+            final LeafTransformSpec transformSpec = new LeafTransformSpec(SEMDistortionTransformA.class.getName(),
+                                                                          testArgs[1]);
+            final List<TransformSpec> tileTransforms = Collections.singletonList(transformSpec);
+            showTile(client, tileTransforms, parameters.scale, testArgs[0], p);
+            showTile(client, tileTransforms, parameters.scale, testArgs[0], q);
         }
 
         SimpleMultiThreading.threadHaltUnClean();
@@ -124,53 +117,6 @@ public class RenderTileWithTransformsClientTest {
                 client.renderTile(tileId, tileTransforms, scale, canvasId, null);
         final ImageProcessor croppedTile = quickCropMaskedArea(ipwm);
         new ImagePlus(testName + "__" + tileId, croppedTile).show();
-    }
-
-    public static OrderedCanvasIdPair buildPair(final String pId,
-                                                final String qId) throws IllegalArgumentException {
-
-        final Matcher pMatcher = TILE_ID_PATTERN.matcher(pId);
-        final Matcher qMatcher = TILE_ID_PATTERN.matcher(qId);
-
-        if (! (pMatcher.matches() && qMatcher.matches())) {
-            throw new IllegalArgumentException("pId " + pId + " and qId "+ qId + " do not match FIB-SEM tile pattern");
-        }
-
-        final int pRow = Integer.parseInt(pMatcher.group(1));
-        final int pColumn = Integer.parseInt(pMatcher.group(2));
-        final String pGroupId = pMatcher.group(3);
-
-        final int qRow = Integer.parseInt(qMatcher.group(1));
-        final int qColumn = Integer.parseInt(qMatcher.group(2));
-        final String qGroupId = qMatcher.group(3);
-
-        if (! pGroupId.equals(qGroupId)) {
-            throw new IllegalArgumentException("pId " + pId + " and qId "+ qId + " must be in same z-layer");
-        }
-
-        if ((pRow != qRow) && (pColumn != qColumn)) {
-            throw new IllegalArgumentException("pId " + pId + " and qId "+ qId + " must be in same row or column");
-        }
-
-        final MontageRelativePosition pPos;
-        final MontageRelativePosition qPos;
-        if (pColumn < qColumn) {
-            pPos = MontageRelativePosition.LEFT;
-            qPos = MontageRelativePosition.RIGHT;
-        } else if (qColumn < pColumn) {
-            qPos = MontageRelativePosition.LEFT;
-            pPos = MontageRelativePosition.RIGHT;
-        } else if (pRow < qRow) {
-            pPos = MontageRelativePosition.TOP;
-            qPos = MontageRelativePosition.BOTTOM;
-        } else { // qRow < pRow
-            qPos = MontageRelativePosition.TOP;
-            pPos = MontageRelativePosition.BOTTOM;
-        }
-
-        return new OrderedCanvasIdPair(new CanvasId(pGroupId, pId, pPos),
-                                       new CanvasId(qGroupId, qId, qPos),
-                                       0.0);
     }
 
     /**
@@ -215,6 +161,4 @@ public class RenderTileWithTransformsClientTest {
 
         return croppedTile;
     }
-
-    private static final Pattern TILE_ID_PATTERN = Pattern.compile(".*_0-(\\d)-(\\d)\\.(?:patch\\.)?(\\d++)\\.0");
 }
