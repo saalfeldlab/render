20e51b078aa6acdfa62bf15e501c5c74220a2fed
Eric Trautman <trautmane@janelia.hhmi.org>
2023-01-06 16:39:51 -0500

    make converter public so that it can be used elsewhere

--- render-app/src/main/java/org/janelia/alignment/ByteRenderer.java
+++ render-app/src/main/java/org/janelia/alignment/ByteRenderer.java
@@ -85,6 +85,6 @@ public class ByteRenderer {
         return new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_BYTE_GRAY);
     };
 
-    private static final Renderer.ProcessorWithMasksConverter CONVERTER =
+    public static final Renderer.ProcessorWithMasksConverter CONVERTER =
             (renderParameters, renderedImageProcessorWithMasks) -> targetToByteImage(renderedImageProcessorWithMasks);
 }
