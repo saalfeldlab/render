f2bb005800acada80f584efc641180c2c8f7166d
Eric Trautman <trautmane@janelia.hhmi.org>
2023-02-17 14:03:42 -0500

    move cross layer intensity correction for ocellar volume into separate class

--- render-ws-java-client/src/main/java/org/janelia/render/client/intensityadjust/IntensityCorrectionWorker.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/intensityadjust/IntensityCorrectionWorker.java
@@ -1,6 +1,5 @@
 package org.janelia.render.client.intensityadjust;
 
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,14 +11,11 @@ import java.util.concurrent.ExecutionException;
 
 import mpicbg.trakem2.transform.TransformMeshMappingWithMasks;
 
-import org.janelia.alignment.ImageAndMask;
 import org.janelia.alignment.RenderParameters;
 import org.janelia.alignment.Utils;
 import org.janelia.alignment.filter.FilterSpec;
 import org.janelia.alignment.filter.LinearIntensityMap8BitFilter;
 import org.janelia.alignment.spec.Bounds;
-import org.janelia.alignment.spec.ChannelSpec;
-import org.janelia.alignment.spec.LayoutData;
 import org.janelia.alignment.spec.ResolvedTileSpecCollection;
 import org.janelia.alignment.spec.TileSpec;
 import org.janelia.alignment.spec.stack.StackMetaData;
@@ -208,121 +204,6 @@ public class IntensityCorrectionWorker implements Serializable {
             dataClient.setStackState(parameters.intensityCorrectedFilterStack,
                                      StackMetaData.StackState.COMPLETE);
         }
-    }
-
-    public static List<TileSpec> deriveCrossLayerIntensityFilterData(final String baseRenderUrl,
-                                                                     final String renderOwner,
-                                                                     final String renderProject,
-                                                                     final String alignedRenderStack,
-                                                                     final int firstIntegralZ,
-                                                                     final int overlapClipMarginPixels)
-            throws ExecutionException, InterruptedException, IOException {
-
-        LOG.info("deriveCrossLayerIntensityFilterData: entry, firstIntegralZ={}", firstIntegralZ);
-
-        final RenderDataClient dataClient = new RenderDataClient(baseRenderUrl, renderOwner, renderProject);
-
-        @SuppressWarnings("UnnecessaryLocalVariable")
-        final double firstZ = firstIntegralZ;
-        final Rectangle firstBounds = dataClient.getLayerBounds(alignedRenderStack, firstZ).toRectangle();
-
-        final double secondZ = firstZ + 1;
-        final Rectangle secondBounds = dataClient.getLayerBounds(alignedRenderStack, secondZ).toRectangle();
-
-        Rectangle overlap = firstBounds.intersection(secondBounds);
-
-        final List<TileSpec> correctedTileSpecs = new ArrayList<>();
-
-        if (! overlap.isEmpty()) {
-            final int margin2x = 2 * overlapClipMarginPixels;
-            final boolean clipWidth = overlap.width > margin2x;
-            final boolean clipHeight = overlap.height > margin2x;
-            overlap = new Rectangle(clipWidth ? overlap.x + overlapClipMarginPixels : overlap.x,
-                                    clipHeight ? overlap.y + overlapClipMarginPixels : overlap.y,
-                                    clipWidth ? overlap.width - overlapClipMarginPixels : overlap.width,
-                                    clipHeight ? overlap.height - overlapClipMarginPixels : overlap.height);
-
-            // make cache large enough to hold shared mask processors
-            final ImageProcessorCache imageProcessorCache =
-                    new ImageProcessorCache(15_000L * 15_000L,
-                                            false,
-                                            false);
-
-            final int numCoefficients = AdjustBlock.DEFAULT_NUM_COEFFICIENTS;
-
-            final String stackUrl = dataClient.getUrls().getStackUrlString(alignedRenderStack);
-
-            final TileSpec firstOverlapSpec = buildOverlapTileSpec(overlap, firstZ, stackUrl);
-            final TileSpec secondOverlapSpec = buildOverlapTileSpec(overlap, secondZ, stackUrl);
-
-            final List<MinimalTileSpecWrapper> alignedOverlapBoxes = new ArrayList<>();
-            alignedOverlapBoxes.add(new MinimalTileSpecWrapper(firstOverlapSpec));
-            alignedOverlapBoxes.add(new MinimalTileSpecWrapper(secondOverlapSpec));
-
-            final ArrayList<OnTheFlyIntensity> corrected =
-                    AdjustBlock.correctIntensitiesForSliceTiles(alignedOverlapBoxes,
-                                                                imageProcessorCache,
-                                                                numCoefficients);
-
-            for (final OnTheFlyIntensity onTheFlyIntensity : corrected) {
-                final TileSpec tileSpec = onTheFlyIntensity.getMinimalTileSpecWrapper().getTileSpec();
-                final LinearIntensityMap8BitFilter filter =
-                        new LinearIntensityMap8BitFilter(numCoefficients,
-                                                         numCoefficients,
-                                                         2,
-                                                         onTheFlyIntensity.getCoefficients());
-                final FilterSpec filterSpec = new FilterSpec(filter.getClass().getName(),
-                                                             filter.toParametersMap());
-                tileSpec.setFilterSpec(filterSpec);
-                tileSpec.convertSingleChannelSpecToLegacyForm();
-                correctedTileSpecs.add(tileSpec);
-            }
-        } else {
-            LOG.warn("deriveCrossLayerIntensityFilterData: stack {} z {} and {} do not overlap - is the stack aligned?",
-                     alignedRenderStack, firstZ, secondZ);
-        }
-
-        return correctedTileSpecs;
-    }
-
-    public static TileSpec buildOverlapTileSpec(final Rectangle overlap,
-                                                final double z,
-                                                final String stackUrl) {
-        final TileSpec tileSpec = new TileSpec();
-        final String zString = String.valueOf(z);
-        tileSpec.setTileId("z_" + zString + "_overlap");
-        tileSpec.setLayout(new LayoutData(zString,
-                                          null,
-                                          null,
-                                          0,
-                                          0,
-                                          0.0,
-                                          0.0,
-                                          0.0));
-        tileSpec.setZ(z);
-        tileSpec.setBoundingBox(new Rectangle(0, 0, (int) overlap.getWidth(), (int) overlap.getHeight()),
-                                tileSpec.getMeshCellSize());
-        tileSpec.setWidth(overlap.getWidth());
-        tileSpec.setHeight(overlap.getHeight());
-
-        // Notes:
-        // - Use png instead of tif for URL because ImageJ Opener loads URL stream twice for tif URLs!
-        //   Doesn't make much difference for normal small tiles, but really slows things down for
-        //   intensity corrected stacks with large tile areas.
-        // - URLs need to have fake format=.png suffixes (which render-ws ignores) so that ImageJ Opener can handle them
-
-        // Example core URL:
-        //   http://renderer-dev.int.janelia.org:8080/render-ws/v1/owner/reiser/project/Z0422_05_Ocellar/stack/v7_acquire_align_ic/z/5827/box/-5000,-4500,8000,6000,0.1/png-image
-        final String overlapUrl = stackUrl + "/z/" + z + "/box/" +
-                                  overlap.x + "," + overlap.y + "," + overlap.width + "," + overlap.height +
-                                  ",1.0/png-image?format=.png";
-
-        final ImageAndMask imageAndMask = new ImageAndMask(overlapUrl, null);
-        final ChannelSpec channelSpec = new ChannelSpec();
-        channelSpec.putMipmap(0, imageAndMask);
-        tileSpec.addChannel(channelSpec);
-
-        return tileSpec;
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(IntensityCorrectionWorker.class);
