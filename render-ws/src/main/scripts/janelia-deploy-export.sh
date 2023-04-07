93ff7bfc64eaae17de71068cabff11227d1749a4
Eric Trautman <trautmane@janelia.hhmi.org>
2023-04-07 08:18:50 -0400

    add memory check and improve logging for janelia deploy export script

--- render-ws/src/main/scripts/janelia-deploy-export.sh
+++ render-ws/src/main/scripts/janelia-deploy-export.sh
@@ -8,18 +8,33 @@ ABSOLUTE_SCRIPT=$(readlink -m "${0}")
 SCRIPTS_DIR=$(dirname "${ABSOLUTE_SCRIPT}")
 DEFAULT_EXPORT_DIR=$(dirname "${SCRIPTS_DIR}")
 
-USAGE="
-USAGE: ${0} [export dir] [install dir] [jetty memory]
-            e.g. /opt/local/docker_exports/export-ibeam_msem-20230404_1322-8c5d66be /opt/local 15g
-"
+USAGE="USAGE: ${0} [jetty gigibyte memory] [install dir] [export dir]
 
-FULL_EXPORT_DIR="${1:-${DEFAULT_EXPORT_DIR}}" # /opt/local/docker_exports/export-ibeam_msem-20230404_1322-8c5d66be
+       $0 3
+       $0 3 /opt/local
+       $0 3 /opt/local /opt/local/docker_exports/export-ibeam_msem-20230404_1322-8c5d66be"
+
+JETTY_GIGI_MEMORY="${1:-15}"
 INSTALL_PARENT_DIR="${2:-/opt/local}"
-JETTY_MEMORY="${3:15g}"
+FULL_EXPORT_DIR="${3:-${DEFAULT_EXPORT_DIR}}"
 
 RENDER_WS_WAR_FILE="${FULL_EXPORT_DIR}/jetty_base/webapps/render-ws.war"
 if [ ! -f "${RENDER_WS_WAR_FILE}" ]; then
-  echo "ERROR: ${RENDER_WS_WAR_FILE} not found ${USAGE}"
+  echo "
+ERROR: ${RENDER_WS_WAR_FILE} not found
+
+${USAGE}
+"
+fi
+
+AVAILABLE_MEMORY=$(free --gibi | awk '/^Mem/{print $3 + $4}')
+if (( AVAILABLE_MEMORY < JETTY_GIGI_MEMORY )); then
+  echo "
+ERROR: requested ${JETTY_GIGI_MEMORY}g for jetty but this machine only has ${AVAILABLE_MEMORY}g ( see free -g )
+
+${USAGE}
+"
+  exit 1
 fi
 
 JDK_VARS="${SCRIPTS_DIR}/jdk-vars.sh"
@@ -39,11 +54,13 @@ JAVA="${INSTALL_PARENT_DIR}/${JDK_VERSION}/bin/java"
 if [ -f "${JAVA}" ]; then
   echo "found ${JAVA}, skipping JDK install"
 else
+  echo "installing JDK from ${JETTY_URL} ..."
   sudo curl "${JETTY_URL}" | sudo tar xz
 fi
 
 # ----------------------------
 # install Jetty stuff
+echo "setting up Jetty in ${INSTALL_PARENT_DIR} ..."
 
 # change permissions on install directory so that jetty_home and jetty_base symbolic links can be created
 sudo chmod 777 "${INSTALL_PARENT_DIR}"
@@ -66,6 +83,8 @@ sudo chmod 755 "${INSTALL_PARENT_DIR}"
 # create work directory
 sudo mkdir -p "${JETTY_WORK}"
 
+echo "setting up Jetty to run as service with jboss user ..."
+
 # change ownership to legacy jbossadmin/jboss setup to ensure access to network mounted images
 sudo chown -R jbossadmin:jboss "${FULL_EXPORT_DIR}"
 sudo chown -R jboss:jboss "${JETTY_LOGS}" "${JETTY_WORK}"
@@ -81,13 +100,25 @@ sudo sed -i '
 # make render-ws.war writable to all to simplify future updates (obviously, don't do this if you are worried about security)
 sudo chmod 666 "${JETTY_BASE}"/webapps/render-ws.war
 
+JETTY_DEFAULT_ENV_FILE="/etc/default/jetty"
+echo "
+creating ${JETTY_DEFAULT_ENV_FILE} with:
+"
+
 sudo echo "JAVA=${INSTALL_PARENT_DIR}/${JDK_VERSION}/bin/java
-JAVA_OPTIONS=\"-Xms${JETTY_MEMORY}g -Xmx${JETTY_MEMORY}g -server -Djava.awt.headless=true\"
+JAVA_OPTIONS=\"-Xms${JETTY_GIGI_MEMORY}g -Xmx${JETTY_GIGI_MEMORY}g -server -Djava.awt.headless=true\"
 JETTY_HOME=${JETTY_HOME}
 JETTY_BASE=${JETTY_BASE}
 JETTY_RUN=${JETTY_LOGS}
-JETTY_STATE=${JETTY_LOGS}/jetty.state" | sudo tee /etc/default/jetty
+JETTY_STATE=${JETTY_LOGS}/jetty.state" | sudo tee "${JETTY_DEFAULT_ENV_FILE}"
 
 # setup jetty as a service that gets automatically restarted after reboots
 sudo cp "${JETTY_BASE}"/jetty.service /etc/systemd/system/jetty.service
 sudo systemctl enable jetty
+
+echo "
+enabled jetty service to be restarted after reboots
+
+to start service, run:
+  sudo systemctl start jetty
+"
