e7f7dda09aa1d273d04a25c323b1cb4b0950cc91
Eric Trautman <trautmane@janelia.hhmi.org>
2023-05-09 20:14:39 -0400

    make IntensityCorrectionStrategy serializable for spark

--- render-ws-java-client/src/main/java/org/janelia/render/client/intensityadjust/IntensityCorrectionStrategy.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/intensityadjust/IntensityCorrectionStrategy.java
@@ -6,11 +6,12 @@ import mpicbg.models.Tile;
 import org.janelia.render.client.intensityadjust.intensity.PointMatchFilter;
 import org.janelia.render.client.intensityadjust.virtual.OnTheFlyIntensity;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public interface IntensityCorrectionStrategy {
+public interface IntensityCorrectionStrategy extends Serializable {
 
 	<T extends Model<T>> T getModelFor(MinimalTileSpecWrapper p);
 
