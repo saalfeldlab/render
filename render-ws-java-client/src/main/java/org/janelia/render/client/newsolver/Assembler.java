6997721cafe7a185924dc6622456c813e59f7990
Stephan Preibisch <stephan.preibisch@gmx.de>
2023-07-31 16:30:51 -0400

    add method specification

--- render-ws-java-client/src/main/java/org/janelia/render/client/newsolver/Assembler.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/newsolver/Assembler.java
@@ -1,6 +1,8 @@
 package org.janelia.render.client.newsolver;
 
+import java.util.List;
+
 public abstract class Assembler
 {
-	public abstract void assemble();
+	public abstract void assemble( List< BlockData > blockDataList );
 }
