35ca0162d6d224e14c044cf260a052f09243263b
Stephan Preibisch <stephan.preibisch@gmx.de>
2023-07-31 20:28:43 -0400

    thinking about BlockData and how to separate geometric from solve-specific data

--- render-ws-java-client/src/main/java/org/janelia/render/client/newsolver/BlockData.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/newsolver/BlockData.java
@@ -1,6 +1,24 @@
 package org.janelia.render.client.newsolver;
 
+/**
+ * Should contain only geometric data, nothing specific to the type of solve
+ * Will need to add this parameter object later rather than extending the class I think
+ * 
+ * @author preibischs
+ *
+ */
 public class BlockData
 {
+	Object solveTypeParameters;
+
 	public int getId() { return -1; }
+	public int getWeight( final double location, final int dim ) { return -1; }
+	public double getCenter( final int dim ) { return -1; }
+
+	public Worker createWorker()
+	{
+		// should maybe ask the solveTypeParamters to create the object I think
+		return null;
+	}
+	
 }
