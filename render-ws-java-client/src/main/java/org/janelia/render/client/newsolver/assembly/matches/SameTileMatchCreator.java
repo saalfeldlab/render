fb6285e70d664f54dcbaa4d71e805c6425432bde
Stephan Preibisch <stephan.preibisch@gmx.de>
2023-08-19 09:23:44 -0400

    TODO for discussing the new method that provides BlockData context in SameTileMatchCreator.java

--- render-ws-java-client/src/main/java/org/janelia/render/client/newsolver/assembly/matches/SameTileMatchCreator.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/newsolver/assembly/matches/SameTileMatchCreator.java
@@ -19,10 +19,12 @@ public interface SameTileMatchCreator< R >
 	 */
 	public void addMatches(
 			TileSpec tileSpec,
-			R modelA,
-			R modelB,
+			R modelA, // TODO: Pair< BlockDataA, modelA >
+			R modelB, // TODO: Pair< BlockDataB, modelB >
 			List< PointMatch > matchesAtoB );
 
+	// TODO: IMPORTANT: you need both blocks, blockA for modelA and blockB for modelB, no (see above)
+	// TODO: that would also get rid of the method below that is just needed for some implementations and seems incomplete
 	/**
 	 * Set the block context in which the matches are created - this is required for the intensity based match creator
 	 * @param blockContext - the block that is queried for context data
