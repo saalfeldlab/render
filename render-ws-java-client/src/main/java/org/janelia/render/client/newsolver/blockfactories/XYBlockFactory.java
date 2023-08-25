708cccb46e28cc74a00e8410a57ff77732505763
Stephan Preibisch <stephan.preibisch@gmx.de>
2023-08-25 09:14:12 -0400

    first potentially working XYBlockFactory (not tested, weight functions are missing)

--- render-ws-java-client/src/main/java/org/janelia/render/client/newsolver/blockfactories/XYBlockFactory.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/newsolver/blockfactories/XYBlockFactory.java
@@ -2,14 +2,17 @@ package org.janelia.render.client.newsolver.blockfactories;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
+import org.janelia.alignment.spec.ResolvedTileSpecCollection;
 import org.janelia.render.client.RenderDataClient;
 import org.janelia.render.client.newsolver.BlockCollection;
 import org.janelia.render.client.newsolver.BlockData;
+import org.janelia.render.client.newsolver.blockfactories.ZBlockFactory.ZBlockInit;
 import org.janelia.render.client.newsolver.blocksolveparameters.BlockDataSolveParameters;
 
-public class XYBlockFactory implements BlockFactory< ZBlockFactory >, Serializable
+public class XYBlockFactory implements BlockFactory< XYBlockFactory >, Serializable
 {
 	private static final long serialVersionUID = -2022190797935740332L;
 
@@ -39,9 +42,14 @@ public class XYBlockFactory implements BlockFactory< ZBlockFactory >, Serializab
 	}
 
 	@Override
-	public <M, R, P extends BlockDataSolveParameters<M, R, P>> BlockCollection<M, R, P, ZBlockFactory> defineBlockCollection(
+	public <M, R, P extends BlockDataSolveParameters<M, R, P>> BlockCollection<M, R, P, XYBlockFactory> defineBlockCollection(
 			final ParameterProvider<M, R, P> blockSolveParameterProvider )
 	{
+		// we use the same code as for Z ...
+		// the LEFT sets are one more than the RIGHT sets and now simply form a grid of X*Y
+		final List< ZBlockInit > initBlocksX = ZBlockFactory.defineBlockLayout( minX, maxX, blockSizeX, minBlockSizeX );
+		final List< ZBlockInit > initBlocksY = ZBlockFactory.defineBlockLayout( minY, maxY, blockSizeY, minBlockSizeY );
+
 		final BlockDataSolveParameters< ?,?,? > basicParameters = blockSolveParameterProvider.basicParameters();
 
 		//
@@ -52,13 +60,69 @@ public class XYBlockFactory implements BlockFactory< ZBlockFactory >, Serializab
 				basicParameters.owner(),
 				basicParameters.project() );
 
+		final ArrayList< BlockData< M, R, P, XYBlockFactory > > blockDataList = new ArrayList<>();
 
-		// TODO Auto-generated method stub
-		return null;
+		// for each block, we know the z-range
+		for ( int y = 0; y < initBlocksY.size(); ++y )
+		{
+			final ZBlockInit initBlockY = initBlocksY.get( y );
+
+			System.out.println( "Y: "+ initBlockY.id + ": " + initBlockY.minZ() + " >> " + initBlockY.maxZ() + " [#"+(initBlockY.maxZ()-initBlockY.minZ()+1) + "]" );
+
+			for ( int x = 0; x < initBlocksX.size(); ++x )
+			{
+				final ZBlockInit initBlockX = initBlocksY.get( x );
+
+				// only RIGHT-RIGHT and LEFT-LEFT are combined
+				if ( initBlockX.location() != initBlockY.location() )
+					continue;
+
+				System.out.println( "   X: "+ initBlockX.id + ": " + initBlockX.minZ() + " >> " + initBlockX.maxZ() + " [#"+(initBlockX.maxZ()-initBlockX.minZ()+1) + "]" );
+
+				try
+				{
+					// TODO: trautmane
+					// we fetch all TileSpecs for our x,y,z-range
+					final ResolvedTileSpecCollection rtsc =
+							r.getResolvedTiles(
+									basicParameters.stack(),
+									(double)minZ,
+									(double)maxZ,
+									null,//groupId,
+									(double)initBlockX.minZ(),
+									(double)initBlockX.maxZ(),
+									(double)initBlockY.minZ(),
+									(double)initBlockY.maxZ(),
+									null );// matchPattern
+
+					System.out.println( "Loaded " + rtsc.getTileIds().size() + " tiles.");
+
+					final int id = y * initBlocksX.size() + x;
+
+					final BlockData< M, R, P, XYBlockFactory > block = 
+							new BlockData<>(
+									this,
+									blockSolveParameterProvider.create( rtsc ),
+									id,
+									rtsc );
+	
+					blockDataList.add( block );
+				}
+				catch (final Exception e)
+				{
+					System.out.println( "Failed to fetch data from render. stopping.");
+					e.printStackTrace();
+					return null;
+				}
+			}
+
+		}
+
+		return new BlockCollection<>( blockDataList );
 	}
 
 	@Override
-	public ArrayList<Function<Double, Double>> createWeightFunctions( final BlockData<?, ?, ?, ZBlockFactory> block )
+	public ArrayList<Function<Double, Double>> createWeightFunctions( final BlockData<?, ?, ?, XYBlockFactory> block )
 	{
 		// TODO Auto-generated method stub
 		return null;
