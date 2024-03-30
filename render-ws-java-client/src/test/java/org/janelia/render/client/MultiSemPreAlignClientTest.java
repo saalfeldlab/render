75354efe0f0b5ab2a0881eecffcf07b62837ab29
Michael Innerberger <innerbergerm@hhmi.org>
2024-03-30 17:47:16 -0400

    Add test for client

---
+++ render-ws-java-client/src/test/java/org/janelia/render/client/MultiSemPreAlignClientTest.java
@@ -0,0 +1,39 @@
+package org.janelia.render.client;
+
+import org.janelia.render.client.parameter.CommandLineParameters;
+import org.junit.Test;
+
+/**
+ * Tests the {@link MultiSemPreAlignClient} class.
+ *
+ * @author Michael Innerberger
+ */
+public class MultiSemPreAlignClientTest {
+
+    @Test
+    public void testParameterParsing() throws Exception {
+        CommandLineParameters.parseHelp(new MultiSemPreAlignClient.Parameters());
+    }
+
+    public static void main(final String[] args) {
+        final String[] effectiveArgs = {
+                "--baseDataUrl", "http://renderer-dev.int.janelia.org:8080/render-ws/v1",
+                "--owner", "hess_wafer_53",
+                "--project", "cut_000_to_009",
+                "--stack", "c009_s310_v01_mfov_08",
+                "--targetStack", "c009_s310_v01_mfov_08_mfov_prealign",
+                "--matchOwner", "hess_wafer_53",
+                "--matchCollection", "c009_s310_v01_match",
+                "--completeTargetStack",
+                "--maxAllowedError", "10.0",
+                "--maxIterations", "1000",
+                "--maxPlateauWidth", "250",
+                "--maxNumMatches", "100",
+                "--numThreads", "8"
+        };
+
+        MultiSemPreAlignClient.main(effectiveArgs);
+    }
+
+
+}
