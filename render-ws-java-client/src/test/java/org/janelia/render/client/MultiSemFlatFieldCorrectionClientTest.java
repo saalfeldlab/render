37b907b7185b86b368bfef7cb1633aaf5f74580a
Michael Innerberger <innerbergerm@hhmi.org>
2024-04-24 16:17:48 -0400

    Change test parameters for client

--- render-ws-java-client/src/test/java/org/janelia/render/client/MultiSemFlatFieldCorrectionClientTest.java
+++ render-ws-java-client/src/test/java/org/janelia/render/client/MultiSemFlatFieldCorrectionClientTest.java
@@ -19,11 +19,11 @@ public class MultiSemFlatFieldCorrectionClientTest {
         args = new String[] {
                 "--baseDataUrl", "http://renderer-dev.int.janelia.org:8080/render-ws/v1",
                 "--owner", "hess_wafer_53d",
-                "--project", "slab_000_to_009",
-                "--stackPattern", "s000_.*",
+                "--project", "slab_070_to_079",
+                "--stackPattern", "s070_m104",
                 "--targetStackSuffix", "_corrected",
                 "--inputRoot", "/nrs/hess/data/hess_wafer_53/raw/imaging/msem",
-                "--outputRoot", "/nrs/hess/render/flatfield/test/data",
+                "--outputRoot", "/nrs/hess/render/flatfield/test/corrected",
                 "--flatFieldLocation", "/nrs/hess/render/flatfield/flatfields_n2000",
                 "--flatFieldFormat", "flat_field_z%03d_sfov%03d_n2000.tif",
         };
