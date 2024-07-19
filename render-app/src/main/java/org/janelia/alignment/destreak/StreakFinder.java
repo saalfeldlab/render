006a9706b71cc451fcfc2a74866dba3d0222adbc
Michael Innerberger <innerbergerm@hhmi.org>
2024-07-19 11:51:20 -0400

    Pull masking into a separate method

--- render-app/src/main/java/org/janelia/alignment/destreak/StreakFinder.java
+++ render-app/src/main/java/org/janelia/alignment/destreak/StreakFinder.java
@@ -6,12 +6,23 @@ import ij.ImagePlus;
 import ij.Prefs;
 
 public class StreakFinder {
-	public static void main(final String[] args) {
-		new ImageJ();
-		final String meanFilterCoefficients = "text1=" + "1\n".repeat(201) + " normalize";
+	private static final int MEAN_FILTER_SIZE = 201;
 
-		final ImagePlus backup = IJ.openImage("/home/innerbergerm@hhmi.org/big-data/streak-correction/jrc_mus-liver-zon-3/z00032-0-0-1.png");
-		final ImagePlus imp = IJ.openImage("/home/innerbergerm@hhmi.org/big-data/streak-correction/jrc_mus-liver-zon-3/z00032-0-0-1.png");
+	public static void main(final String[] args) {
+		final String srcPath = "/home/innerbergerm@hhmi.org/big-data/streak-correction/jrc_mus-liver-zon-3/z00032-0-0-1.png";
+
+		final ImagePlus backup = new ImagePlus(srcPath);
+		final ImagePlus imp = maskStreaks(srcPath);
+
+		new ImageJ();
+		imp.show();
+		backup.show();
+	}
+
+	private static ImagePlus maskStreaks(final String srcPath) {
+		final ImagePlus imp = new ImagePlus(srcPath);
+		final String meanFilterCoefficients = "text1=" + "1\n".repeat(MEAN_FILTER_SIZE) + " normalize";
+
 		IJ.run(imp, "32-bit", "");
 		IJ.run(imp, "Convolve...", "text1=[-1 0 1\n] normalize");
 		IJ.run(imp, "Convolve...", meanFilterCoefficients);
@@ -20,8 +31,6 @@ public class StreakFinder {
 		Prefs.blackBackground = false;
 		IJ.run(imp, "Convert to Mask", "");
 		IJ.run(imp, "Gaussian Blur...", "sigma=3");
-
-		imp.show();
-		backup.show();
+		return imp;
 	}
 }
