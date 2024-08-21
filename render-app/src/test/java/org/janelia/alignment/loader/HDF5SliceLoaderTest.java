e80542f25fe26afd44c62770ae44bf1ad34eff98
Eric Trautman <trautmane@janelia.hhmi.org>
2024-08-21 15:35:56 -0400

    Bump n5 version to 3.2.0 to fix HDF5 read issue introduced by using scijava 38.0.1 with older n5 3.0.2.  Added test to catch similar problems in the future.  Note that the hot-knife repo will need to bump its scijava version to 38.0.1 before it can be used to process anything exported from render after this commit.

--- render-app/src/test/java/org/janelia/alignment/loader/HDF5SliceLoaderTest.java
+++ render-app/src/test/java/org/janelia/alignment/loader/HDF5SliceLoaderTest.java
@@ -2,12 +2,30 @@ package org.janelia.alignment.loader;
 
 import ij.process.ImageProcessor;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Tests the {@link HDF5SliceLoader} class.
  *
  * @author Eric Trautman
  */
 public class HDF5SliceLoaderTest {
+
+    @Test
+    public void testLoad() {
+        final HDF5SliceLoader loader = new HDF5SliceLoader();
+        final String urlString = "src/test/resources/hdf5-test/Merlin-6284_24-02-27_180604.uint8.h5?dataSet=/0-0-0/mipmap.0&z=0";
+        ImageProcessor ip = null;
+        try {
+            ip = loader.load(urlString);
+        } catch (final Throwable t) {
+            final String failureMessage = ExceptionUtils.getStackTrace(t);
+            Assert.fail("failed load with following exception: " + failureMessage);
+        }
+        Assert.assertEquals("invalid width", 5000, ip.getWidth());
+    }
 
     public static void main(final String[] args) {
         final HDF5SliceLoader loader = new HDF5SliceLoader();
