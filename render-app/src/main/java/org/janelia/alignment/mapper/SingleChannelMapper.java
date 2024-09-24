3023338d70ce629e9a6c3071dd5f8fd751308304
Eric Trautman <trautmane@janelia.hhmi.org>
2024-09-24 17:33:05 -0400

    added commented out blocks for other subsample variants

--- render-app/src/main/java/org/janelia/alignment/mapper/SingleChannelMapper.java
+++ render-app/src/main/java/org/janelia/alignment/mapper/SingleChannelMapper.java
@@ -34,6 +34,14 @@ public class SingleChannelMapper
 	final AffineTransform2D tInv;
 	final double[] tmp;
 
+    // 2x2 subsampling using top-left pixels
+//    final int subsampling = 2;
+//    final long[] offset = new long[] { 0, 0 };
+
+    // 2x2 subsampling using bottom-right pixels
+//    final int subsampling = 2;
+//    final long[] offset = new long[] { -1, -1 };
+
 	// 3x3 subsampling using center pixels
 	final int subsampling = 3;
 	final long[] offset = new long[] { -1, -1 };
