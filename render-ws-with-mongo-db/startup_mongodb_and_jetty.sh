cd2058e2c4d1f974552589150b00354163758e35
Eric Trautman <trautmane@janelia.hhmi.org>
2025-01-27 16:22:52 -0500

    first draft of render-ws-with-mongodb dockerfile

--- render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
+++ render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
@@ -3,18 +3,8 @@
 # ------------------------------------------------------------------------------------------------------
 # From https://github.com/saalfeldlab/render/blob/master/docs/src/site/markdown/render-ws.md
 
-echo """
-  Install 7. Start MongoDB
-"""
+# start mongodb
 sudo -u mongodb /usr/bin/mongod -f /etc/mongod.conf &
 
-echo """
-  Install 8. Start Jetty
-"""
-
-echo """
-  Hack: Passing run instead of start to jetty.sh to work-around startup failure.
-        See https://github.com/eclipse/jetty.project/issues/7008 and
-        https://github.com/eclipse/jetty.project/issues/7095
-"""
-deploy/jetty_base/jetty_wrapper.sh run
+# start jetty
+deploy/jetty_base/jetty_wrapper.sh start
