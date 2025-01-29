709a9be73b452fc6f3c9f5309cd305a2f3d1706d
Eric Trautman <trautmane@janelia.hhmi.org>
2025-01-28 19:27:03 -0500

    first draft of render-ws-with-mongodb dockerfile

--- render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
+++ render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
@@ -3,8 +3,11 @@
 # ------------------------------------------------------------------------------------------------------
 # From https://github.com/saalfeldlab/render/blob/master/docs/src/site/markdown/render-ws.md
 
-# start mongodb
+echo "starting mongodb"
 sudo -u mongodb /usr/bin/mongod -f /etc/mongod.conf &
 
-# start jetty
+echo "starting jetty"
 deploy/jetty_base/jetty_wrapper.sh start
+
+echo "sleeping 'infinity' to keep container running"
+sleep infinity
