11da805599ce9b6234b87cac82c073884880b4c6
Eric Trautman <trautmane@janelia.hhmi.org>
2025-09-19 14:20:38 -0400

    add support for environmental override of jetty_wrapper.sh memory limit

--- render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
+++ render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
@@ -6,6 +6,9 @@
 echo "starting mongodb"
 sudo -u mongodb /usr/bin/mongod -f /etc/mongod.conf &
 
+# default memory to 28g assuming that we have a 32g VM
+export RENDER_JETTY_MIN_AND_MAX_MEMORY=${1:-28g}
+
 # note that jetty_wrapper.sh start still does not seem to work and we want to keep the container active anyway
 echo "starting jetty_wrapper.sh run (which should keep the container active)"
 deploy/jetty_base/jetty_wrapper.sh run
--- render-ws/src/main/scripts/jetty/jetty_wrapper.sh
+++ render-ws/src/main/scripts/jetty/jetty_wrapper.sh
@@ -2,7 +2,12 @@
  
 #------------------------------------------------------------------------------------------------------
 # Wrapper script to setup environment variables before calling real Jetty server script (bin/jetty.sh).
+# Default web server memory limit is 3g (intended for a 4g server).
+# Override the default memory limit by setting the RENDER_JETTY_MIN_AND_MAX_MEMORY environment variable
+# (e.g. "15g" for 16g server or "400g" for 500g server).
 #------------------------------------------------------------------------------------------------------
+
+JETTY_MIN_AND_MAX_MEMORY=${RENDER_JETTY_MIN_AND_MAX_MEMORY:-"3g"}
 
 export JETTY_HOME="/opt/local/jetty_home"
 export JETTY_BASE="/opt/local/jetty_base"
@@ -19,17 +24,7 @@ export JETTY_STATE="${JETTY_RUN}/jetty.state"
 export JAVA_HOME="/misc/sc/jdks/zulu11"
 export PATH="${JAVA_HOME}/bin:${PATH}"
 export JAVA="${JAVA_HOME}/bin/java"
- 
-# Uncomment appropriate options for server ...
- 
-# small 4GB server: 
-export JAVA_OPTIONS="-Xms3g -Xmx3g -server -Djava.awt.headless=true"
- 
-# larger 16GB server
-#export JAVA_OPTIONS="-Xms15g -Xmx15g -server -Djava.awt.headless=true"
- 
-# super 500GB server
-#export JAVA_OPTIONS="-Xms400g -Xmx400g -server -Djava.awt.headless=true"
+export JAVA_OPTIONS="-Xms${JETTY_MIN_AND_MAX_MEMORY} -Xmx${JETTY_MIN_AND_MAX_MEMORY} -server -Djava.awt.headless=true"
  
 # run the real script ... 
-${JETTY_HOME}/bin/jetty.sh $* 2>&1 1>>${JETTY_RUN}/jetty_bootstrap.log
+${JETTY_HOME}/bin/jetty.sh "$@" 1>>${JETTY_RUN}/jetty_bootstrap.log 2>&1
