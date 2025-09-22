4bf36dc469632d68f6322d86b891b986187bae28
Eric Trautman <trautmane@janelia.hhmi.org>
2025-09-22 15:40:59 -0400

    For mongodb and jetty docker instances, run mongod directly as root instead of using sudo to ensure that the mongod process inherits the open files limits from the process that launches docker.  When using sudo inside a docker container on Google VMs, the soft limit was defaulted to a very low 1024.

--- render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
+++ render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
@@ -4,7 +4,7 @@
 # From https://github.com/saalfeldlab/render/blob/master/docs/src/site/markdown/render-ws.md
 
 echo "starting mongodb"
-sudo -u mongodb /usr/bin/mongod -f /etc/mongod.conf &
+/usr/bin/mongod -f /etc/mongod.conf &
 
 # default memory to 28g assuming that we have a 32g VM
 export RENDER_JETTY_MIN_AND_MAX_MEMORY=${1:-28g}
