31f3ffd32bd039b0a5b713c2139e3bb897b500cd
Eric Trautman <trautmane@janelia.hhmi.org>
2025-09-27 15:17:17 -0400

    increase render-ws-with-mongodb default memory from 28g to 60g for new n2-standard-16 VMs

--- render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
+++ render-ws-with-mongo-db/startup_mongodb_and_jetty.sh
@@ -6,8 +6,8 @@
 echo "starting mongodb"
 /usr/bin/mongod -f /etc/mongod.conf &
 
-# default memory to 28g assuming that we have a 32g VM
-export RENDER_JETTY_MIN_AND_MAX_MEMORY=${1:-28g}
+# default memory to 60g assuming that we have a 64g VM (n2-standard-16)
+export RENDER_JETTY_MIN_AND_MAX_MEMORY=${1:-60g}
 
 # note that jetty_wrapper.sh start still does not seem to work and we want to keep the container active anyway
 echo "starting jetty_wrapper.sh run (which should keep the container active)"
