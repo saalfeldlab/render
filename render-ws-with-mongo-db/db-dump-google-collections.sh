4a193dc0182f917bff5f371268af34000f9076a1
Eric Trautman <trautmane@janelia.hhmi.org>
2025-11-09 08:53:03 -0500

    simplify parameters for db-dump-google-collections.sh

--- render-ws-with-mongo-db/db-dump-google-collections.sh
+++ render-ws-with-mongo-db/db-dump-google-collections.sh
@@ -6,40 +6,44 @@ set -e
 # Generate mongodb collection dump files from a render-ws-mongodb Google Cloud VM container.
 #
 # Dump files for the collections are written to:
-#   /mnt/disks/mongodb_dump_fs/dump/${HOSTNAME}/collections/${RUN_TIMESTAMP}/${DB}/${COLLECTION}.bson.gz
+#   /mnt/disks/mongodb_dump_fs/dump/render-ws-mongodb-16c-64gb-[dump-suffix]/collections/[collection-dir | run-time]
 
-if [ $# != 3 ]; then
+if [ $# -lt 3 ]; then
   echo "
-Usage:    $0 <hostname> <db> <collection-pattern>
+Usage:    $0 <db> <dump-suffix> <collection-pattern> [collection-dir]
 
-Examples: $0  render-ws-mongodb-8c-32gb-abf  render  '.*w60_s360_r00_gc_[mr].*'
-          $0  render-ws-mongodb-8c-32gb-abf  match   '.*w60_s360_r00_gc_.*'
+Examples: $0  render  par    '.*_par_.*'       w61_s140_to_149_par
+          $0  match   match  '.*_s15[5-9]_.*'  w61_s155_to_159
+          $0  render  align  '.*align.*'       w61_s070_to_071_r00_align
+          $0  render  ic2d   '.*ic2d.*'        w61_s100_to_s109_r00_ic2d_nc4_hist
 
-          $0  render-ws-mongodb-8c-32gb-abf  render  '.*w60_s360_r00_(gc|gc_mat|gc_mat_render)__.*'
-
-          $0  render-ws-mongodb-16c-64gb-aaa  render  '.*_pa_.*'
-          $0  render-ws-mongodb-16c-64gb-aaa  match   '.*_s15[0-4]_.*'
-
-          $0  render-ws-mongodb-16c-64gb-align  render  '.*_align.*'
-
+          $0  render  mat    '.*w60_s360_r00_(gc|gc_mat|gc_mat_render)__.*'
 "
   exit 1
 fi
 
-DUMP_SUBDIR="${1}"
-DB="${2}"
-PATTERN="${3}"
+RUN_TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
+DB="${1}"
+BASE_DUMP_SUFFIX="${2}"
+COLLECTION_PATTERN="${3}"
+COLLECTION_DIR="${4:-${RUN_TIMESTAMP}}"
 
 BASE_DUMP_DIR="/mnt/disks/mongodb_dump_fs/dump"
-DUMP_WAIT_SECONDS=3
-
 if [ ! -d "${BASE_DUMP_DIR}" ]; then
   echo "ERROR: ${BASE_DUMP_DIR} not found"
   exit 1
 fi
 
+# /mnt/disks/mongodb_dump_fs/dump/render-ws-mongodb-16c-64gb-align/collections/w61_s100_to_101_r00_align
+FULL_DUMP_DIR="${BASE_DUMP_DIR}/render-ws-mongodb-16c-64gb-${BASE_DUMP_SUFFIX}/collections/${COLLECTION_DIR}"
+if [ -d "${FULL_DUMP_DIR}" ]; then
+  echo "ERROR: ${FULL_DUMP_DIR} already exists"
+  exit 1
+fi
+
 CONNECTION_URI="mongodb://localhost:27017/${DB}"
-EVAL_CMD="printjson(db.getCollectionNames().filter(c => /${PATTERN}/.test(c)).sort());"
+EVAL_CMD="printjson(db.getCollectionNames().filter(c => /${COLLECTION_PATTERN}/.test(c)).sort());"
 
 COLLECTIONS=$( mongosh "${CONNECTION_URI}" --quiet --eval "${EVAL_CMD}" | grep "'" | sed "s/[',]//g" )
 
@@ -48,7 +52,7 @@ COLLECTION_ARRAY=(${COLLECTIONS})
 COLLECTION_COUNT=${#COLLECTION_ARRAY[@]}
 
 if [ "${COLLECTION_COUNT}" -eq 0 ]; then
-  echo "no collections found matching collection-pattern ${PATTERN}"
+  echo "no collections found matching collection-pattern ${COLLECTION_PATTERN}"
   exit 1
 elif [[ "${COLLECTION_COUNT}" -eq 1 && "${COLLECTION_ARRAY[0]}" = "admin__stack_meta_data" ]]; then
   echo "ERROR: collection admin__stack_meta_data is not allowed by itself, change the collection-pattern"
@@ -68,7 +72,7 @@ if [[ "${DB}" == "render" && "${COLLECTION_COUNT}" -gt 1 ]]; then
 fi
 
 echo "
-Found ${COLLECTION_COUNT} collections matching pattern ${PATTERN}:
+Found ${COLLECTION_COUNT} collections matching pattern ${COLLECTION_PATTERN}:
 "
 unset SMD_QUERY
 COUNT=0
@@ -108,14 +112,13 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   exit 1
 fi
 
-RUN_TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-# /mnt/disks/mongodb_dump_fs/dump/render-ws-mongodb-8c-32gb-abc/collections/20250217_215500
-DUMP_DIR="${BASE_DUMP_DIR}/${DUMP_SUBDIR}/collections/${RUN_TIMESTAMP}"
-mkdir -p "${DUMP_DIR}"
+mkdir -p "${FULL_DUMP_DIR}"
+
+DUMP_WAIT_SECONDS=3
 
 for COLLECTION in ${COLLECTIONS}; do
 
-  mongodump --uri="${CONNECTION_URI}" --db="${DB}" --collection="${COLLECTION}" --gzip --out="${DUMP_DIR}"
+  mongodump --uri="${CONNECTION_URI}" --db="${DB}" --collection="${COLLECTION}" --gzip --out="${FULL_DUMP_DIR}"
 
   if [ "${DB}" == "match" ]; then
     echo "sleeping for ${DUMP_WAIT_SECONDS} seconds in attempt to avoid container crash on larger dumps"
@@ -130,13 +133,13 @@ if [[ -v SMD_QUERY ]]; then
 dumping admin__stack_meta_data with query:
 ${SMD_QUERY}]}
 "
-  echo "${SMD_QUERY}]}" > "${DUMP_DIR}/render/admin__stack_meta_data.query.txt"
-  mongodump --uri="${CONNECTION_URI}" --db="${DB}" --collection=admin__stack_meta_data --query "${SMD_QUERY}]}" --gzip --out="${DUMP_DIR}"
+  echo "${SMD_QUERY}]}" > "${FULL_DUMP_DIR}/render/admin__stack_meta_data.query.txt"
+  mongodump --uri="${CONNECTION_URI}" --db="${DB}" --collection=admin__stack_meta_data --query "${SMD_QUERY}]}" --gzip --out="${FULL_DUMP_DIR}"
 
   COLLECTION_COUNT=$((COLLECTION_COUNT+1))
 fi
 
 echo "
 Dumped ${COLLECTION_COUNT} collections to:
-  ${DUMP_DIR}
+  ${FULL_DUMP_DIR}
 "
