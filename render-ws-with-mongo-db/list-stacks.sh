f535da3a7342603fd4093b310db0b34cb9b41689
Eric Trautman <trautmane@janelia.hhmi.org>
2025-11-14 12:29:34 -0500

    add maxZ values to list-stacks script output

--- render-ws-with-mongo-db/list-stacks.sh
+++ render-ws-with-mongo-db/list-stacks.sh
@@ -26,7 +26,46 @@ fi
 
 printf "\nStacks for owner %s and project %s are:\n\n" "${OWNER}" "${PROJECT}"
 
-STACK_IDS_URL="${OWNER_URL}/project/${PROJECT}/stackIds"
-curl -X GET --silent --header 'Accept: application/json' "${STACK_IDS_URL}" | jq -r '.[] | "\(.stack)"' | sort
+STACKS_URL="${OWNER_URL}/project/${PROJECT}/stacks"
+curl -X GET --silent --header 'Accept: application/json' "${STACKS_URL}" | \
+    jq -r '.[] | "  \(.stackId.stack) with maxZ \(.stats.stackBounds.maxZ)"' | sort
+
+# .../stacks JSON example:
+# {
+#   "stackId": {
+#     "owner": "hess_wafers_60_61",
+#     "project": "w61_serial_100_to_109",
+#     "stack": "w61_s109_r00_gc_par_align"
+#   },
+#   "state": "COMPLETE",
+#   "lastModifiedTimestamp": "2025-10-07T02:54:53.321Z",
+#   "currentVersionNumber": 0,
+#   "currentVersion": {
+#     "createTimestamp": "2025-10-07T00:16:29.550Z",
+#     "versionNotes": "derived from stack with owner 'hess_wafers_60_61', project 'w61_serial_100_to_109', and name 'w61_s109_r00_gc_par'",
+#     "stackResolutionX": 8,
+#     "stackResolutionY": 8,
+#     "stackResolutionZ": 8
+#   },
+#   "stats": {
+#     "stackBounds": {
+#       "minX": -5318,
+#       "minY": -783,
+#       "minZ": 1,
+#       "maxX": 130342,
+#       "maxY": 104672,
+#       "maxZ": 82
+#     },
+#     "sectionCount": 82,
+#     "nonIntegralSectionCount": 0,
+#     "tileCount": 216125,
+#     "transformCount": 0,
+#     "minTileWidth": 1717,
+#     "maxTileWidth": 2368,
+#     "minTileHeight": 1520,
+#     "maxTileHeight": 2009,
+#     "channelNames": []
+#   }
+# }
 
 echo
