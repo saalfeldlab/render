b2c625d118cc57f842190ecd34edba9ecc4bf6ac
Michael Innerberger <innerbergerm@hhmi.org>
2026-08-23 18:43:39 -0400

    Use assertAll where appropriate

--- render-app/src/test/java/org/janelia/alignment/betterbox/BoxDataTest.java
+++ render-app/src/test/java/org/janelia/alignment/betterbox/BoxDataTest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,40 +18,41 @@ public class BoxDataTest {
     @Test
     public void testFromString() throws Exception {
 
-        BoxData boxData = BoxData.fromString("1,2.3,4,5,2,0123");
+        final BoxData firstBox = BoxData.fromString("1,2.3,4,5,2,0123");
 
-        assertEquals(1, boxData.getLevel(), "invalid level parsed");
-        assertEquals(2.3, boxData.getZ(), 0.001, "invalid z parsed");
-        assertEquals(4, boxData.getRow(), "invalid row parsed");
-        assertEquals(5, boxData.getColumn(), "invalid column parsed");
-        assertEquals(2, boxData.getNumberOfSiblings(), "invalid number of siblings parsed");
-        assertEquals(4, boxData.getChildCount(), "invalid number of children parsed");
+        assertAll("invalid first box parsed",
+                  () -> assertEquals(1, firstBox.getLevel(), "invalid level parsed"),
+                  () -> assertEquals(2.3, firstBox.getZ(), 0.001, "invalid z parsed"),
+                  () -> assertEquals(4, firstBox.getRow(), "invalid row parsed"),
+                  () -> assertEquals(5, firstBox.getColumn(), "invalid column parsed"),
+                  () -> assertEquals(2, firstBox.getNumberOfSiblings(), "invalid number of siblings parsed"),
+                  () -> assertEquals(4, firstBox.getChildCount(), "invalid number of children parsed"));
 
-        List<BoxData> children = boxData.getChildren();
-        for (int i = 0; i < children.size(); i++) {
-            final BoxData child = children.get(i);
+        final List<BoxData> firstChildren = firstBox.getChildren();
+        for (int i = 0; i < firstChildren.size(); i++) {
+            final BoxData child = firstChildren.get(i);
             assertNotNull(child, "child " + i + " is null");
-            assertEquals(boxData.getLevel(), child.getParentLevel(),
+            assertEquals(firstBox.getLevel(), child.getParentLevel(),
                          "invalid parent level for child " + i);
-            assertEquals(boxData.getRow(), child.getParentRow(),
+            assertEquals(firstBox.getRow(), child.getParentRow(),
                          "invalid parent row for child " + i);
-            assertEquals(boxData.getColumn(), child.getParentColumn(),
+            assertEquals(firstBox.getColumn(), child.getParentColumn(),
                          "invalid parent column for child " + i);
         }
 
-        boxData = BoxData.fromString("0,99,10,20,1,");
+        final BoxData secondBox = BoxData.fromString("0,99,10,20,1,");
 
-        assertEquals(0, boxData.getLevel(), "invalid level parsed");
-        assertEquals(0, boxData.getChildCount(), "invalid number of children parsed");
+        assertEquals(0, secondBox.getLevel(), "invalid level parsed");
+        assertEquals(0, secondBox.getChildCount(), "invalid number of children parsed");
 
-        boxData = BoxData.fromString("2,99,40,41,1,13");
+        final BoxData thirdBox = BoxData.fromString("2,99,40,41,1,13");
 
-        assertEquals(2, boxData.getLevel(), "invalid level parsed");
-        assertEquals(2, boxData.getChildCount(), "invalid number of children parsed");
+        assertEquals(2, thirdBox.getLevel(), "invalid level parsed");
+        assertEquals(2, thirdBox.getChildCount(), "invalid number of children parsed");
 
-        children = boxData.getChildren();
-        assertEquals(1, children.get(0).getParentIndex(), "invalid parent index for first child");
-        assertEquals(3, children.get(1).getParentIndex(), "invalid parent index for second child");
+        final List<BoxData> thirdChildren = thirdBox.getChildren();
+        assertEquals(1, thirdChildren.get(0).getParentIndex(), "invalid parent index for first child");
+        assertEquals(3, thirdChildren.get(1).getParentIndex(), "invalid parent index for second child");
 
     }
 
