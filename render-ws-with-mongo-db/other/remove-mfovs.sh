44f7eb0f7937021a5753745e759f954bb11dd0a5
Eric Trautman <trautmane@janelia.hhmi.org>
2026-08-24 10:05:47 -0400

    update remove-mfovs.sh for w61_s178_r00_gc_icc_par

--- render-ws-with-mongo-db/other/remove-mfovs.sh
+++ render-ws-with-mongo-db/other/remove-mfovs.sh
@@ -4,8 +4,8 @@ set -e
 
 BASE_URL="http://localhost:8080/render-ws/v1"
 OWNER="hess_wafers_60_61"
-PROJECT="w61_serial_100_to_109"
-STACK="w61_s109_r00_gc_par"
+PROJECT="w61_serial_170_to_179"
+STACK="w61_s178_r00_gc_icc_par"
 
 STACK_URL="${BASE_URL}/owner/${OWNER}/project/${PROJECT}/stack/${STACK}"
 
@@ -14,14 +14,16 @@ curl -s -X GET --header 'Accept: application/json' "${STACK_URL}" | jq '.'
 
 curl -s -X PUT --header 'Content-Type: application/json' --header 'Accept: text/plain' "${STACK_URL}/state/LOADING"
 
-BASE_TILE_IDS_URL="${STACK_URL}/tileIds?minZ=17&maxZ=17"
+for Z in $(seq 1 68); do
+  BASE_TILE_IDS_URL="${STACK_URL}/tileIds?minZ=${Z}&maxZ=${Z}"
 
-for MFOV in m0023 m0026 m0028; do
-  TILE_IDS_URL="${BASE_TILE_IDS_URL}&matchPattern=_${MFOV}"
-  mapfile -t TILE_IDS < <(curl -s "${TILE_IDS_URL}" | jq -r '.[]' | sort)
-  printf "deleting %d tiles for %s ...\n" "${#TILE_IDS[@]}" "${MFOV}"
-  for TILE_ID in "${TILE_IDS[@]}"; do
-    curl -s -X DELETE --header 'Accept: text/plain' "${STACK_URL}/tile/${TILE_ID}"
+  for MFOV in m0016; do
+    TILE_IDS_URL="${BASE_TILE_IDS_URL}&matchPattern=_${MFOV}"
+    mapfile -t TILE_IDS < <(curl -s "${TILE_IDS_URL}" | jq -r '.[]' | sort)
+    printf "deleting %d tiles for %s ...\n" "${#TILE_IDS[@]}" "${MFOV}"
+    for TILE_ID in "${TILE_IDS[@]}"; do
+      curl -s -X DELETE --header 'Accept: text/plain' "${STACK_URL}/tile/${TILE_ID}"
+    done
   done
 done
 
